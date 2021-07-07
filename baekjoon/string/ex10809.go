268d1cb0dc5cdf89eb9d7be717fdbf32e8b50487
thisisnew <thisisnew@naver.com>
2021-07-07 21:22:26 +0900

    백준-문자열 10809번

--- baekjoon/string/ex10809.go
+++ baekjoon/string/ex10809.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
+func main() {
+	var input string
+	var reader = bufio.NewReader(os.Stdin)
+	fmt.Fscanln(reader, &input)
+
+	var alphabet = make(map[int]int)
+	for i := 0; i < 26; i++ {
+		alphabet[i+97] = -1
+	}
+
+	for i := 0; i < len(input); i++ {
+		ch, _ := strconv.Atoi(fmt.Sprintf("%d", input[i]))
+		if alphabet[ch] == -1 {
+			alphabet[ch] = i
+		}
+	}
+
+	for i := 0; i < 26; i++ {
+		fmt.Printf("%d ", alphabet[i+97])
+	}
+	fmt.Print("\n")
 }
