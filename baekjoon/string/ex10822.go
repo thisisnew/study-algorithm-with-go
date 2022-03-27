6e6b726c9d7bdebc925573682216b1f2331e7261
thisisnew <thisisnew@naver.com>
2022-03-27 11:45:43 +0900

    더하기

---
+++ baekjoon/string/ex10822.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	var input string
+	var read = bufio.NewReader(os.Stdin)
+
+	fmt.Fscan(read, &input)
+
+	stringArray := strings.Split(input, ",")
+
+	var total int
+
+	for _, item := range stringArray {
+
+		num, _ := strconv.Atoi(item)
+
+		total += num
+
+	}
+
+	fmt.Println(total)
+}
