fbee1f98b023e5c49b75ebc5144fb2918944f087
thisisnew <thisisnew@naver.com>
2022-03-27 11:54:40 +0900

    생일

---
+++ baekjoon/string/ex5635.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strings"
+)
+
+func main() {
+
+	var num int
+	var read = bufio.NewReader(os.Stdin)
+	fmt.Fscan(read, &num)
+
+	var infoMap map[string]string
+	var birthArray []string
+
+	for i := 0; i < num; i++ {
+		var input string
+		fmt.Fscanln(read, &input)
+
+		infos := strings.Split(input, " ")
+		name := infos[0]
+		birth := fmt.Sprintf("%s%s%s", infos[3], infos[2], infos[1])
+
+		birthArray = append(birthArray, birth)
+		infoMap[birth] = name
+	}
+
+	sort.Slice(birthArray, func(i, j int) bool {
+		return birthArray[i] < birthArray[j]
+	})
+
+	fmt.Println(infoMap[birthArray[0]])
+	fmt.Println(infoMap[birthArray[num-1]])
+}
