30ccd9494ac2e80b699036b709789b93121025b4
thisisnew <thisisnew@naver.com>
2022-03-27 11:46:03 +0900

    더하기

--- baekjoon/string/ex10822.go
+++ baekjoon/string/ex10822.go
@@ -12,7 +12,6 @@ func main() {
 
 	var input string
 	var read = bufio.NewReader(os.Stdin)
-
 	fmt.Fscan(read, &input)
 
 	stringArray := strings.Split(input, ",")
@@ -20,11 +19,9 @@ func main() {
 	var total int
 
 	for _, item := range stringArray {
-
 		num, _ := strconv.Atoi(item)
 
 		total += num
-
 	}
 
 	fmt.Println(total)
