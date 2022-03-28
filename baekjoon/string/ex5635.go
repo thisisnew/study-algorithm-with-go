6758623c7485f5747d781c5995a4f72dfa7c8d49
thisisnew <thisisnew@naver.com>
2022-03-28 22:33:38 +0900

    생일

--- baekjoon/string/ex5635.go
+++ baekjoon/string/ex5635.go
@@ -3,25 +3,39 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"sort"
+	"strconv"
 	"strings"
 )
 
 func main() {
 
-	var num int
-	var read = bufio.NewReader(os.Stdin)
-	fmt.Fscan(read, &num)
-
-	var infoMap map[string]string
+	var infoMap = map[string]string{}
 	var birthArray []string
 
-	for i := 0; i < num; i++ {
-		input, _ := read.ReadString('\n')
+	var num int
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		line, _, err := reader.ReadLine()
+
+		if err == io.EOF {
+			break
+		}
+
+		input := string(line)
 		infos := strings.Split(input, " ")
+
+		if len(infos) < 3 {
+			n, _ := strconv.Atoi(input)
+			num = n
+			continue
+		}
+
 		name := infos[0]
-		birth := fmt.Sprintf("%s%s%s", infos[3], infos[2], infos[1])
+		birth := fmt.Sprintf("%s%s%s", infos[3], addZero(infos[2]), addZero(infos[1]))
 
 		birthArray = append(birthArray, birth)
 		infoMap[birth] = name
@@ -33,4 +47,15 @@ func main() {
 
 	fmt.Println(infoMap[birthArray[0]])
 	fmt.Println(infoMap[birthArray[num-1]])
+}
+
+func addZero(num string) string {
+
+	n, _ := strconv.Atoi(num)
+
+	if n < 10 {
+		num = "0" + num
+	}
+
+	return num
 }
