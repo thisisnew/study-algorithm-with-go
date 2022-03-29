9040923dcb46d56d974cc9d7dbd619b444b73ba0
thisisnew <thisisnew@naver.com>
2022-03-29 23:17:03 +0900

    모음의 개수

---
+++ baekjoon/string/ex10987.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	var input string
+	var read = bufio.NewReader(os.Stdin)
+
+	fmt.Fscan(read, &input)
+
+	var result int
+
+	for _, ch := range input {
+		if isVerb(ch) {
+			result++
+		}
+	}
+
+	fmt.Println(result)
+}
+
+func isVerb(ch rune) bool {
+	var verbs = []rune{'a', 'e', 'i', 'o', 'u'}
+
+	for _, verb := range verbs {
+		if verb == ch {
+			return true
+		}
+	}
+
+	return false
+}
