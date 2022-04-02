fe51539f7e71660f8d0e1f55093ccbaa3f8feaac
thisisnew <thisisnew@naver.com>
2022-04-03 00:24:37 +0900

    정수의 개수

---
+++ baekjoon/string/ex10821.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	var input string
+	var read = bufio.NewReader(os.Stdin)
+
+	fmt.Fscanln(read, &input)
+
+	nums := strings.Split(input, ",")
+
+	fmt.Println(len(nums))
+
+}
