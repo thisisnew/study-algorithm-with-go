400f4fd47d9789f1479168c89af3cb72e5755e17
thisisnew <thisisnew@naver.com>
2022-04-07 22:02:14 +0900

    뒤집기

---
+++ baekjoon/string/ex1439.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	var s string
+	var read = bufio.NewReader(os.Stdin)
+
+	fmt.Fscanln(read, &s)
+
+	var zeroCnt int
+	var oneCnt int
+	var prev = 0
+
+	//0 -> 1변환
+	for i, r := range s {
+		if string(r) == "0" {
+			if i == 0 || (i == prev+1) {
+				zeroCnt++
+			}
+		}
+		prev = i
+	}
+
+	if zeroCnt == 0 || zeroCnt == len(s) {
+		fmt.Println(0)
+		return
+	}
+
+	//1 -> 0
+	prev = 0
+	for i, r := range s {
+		if string(r) == "1" {
+			if i == 0 || (i == prev+1) {
+				oneCnt++
+			}
+		}
+		prev = i
+	}
+
+	if zeroCnt < oneCnt {
+		fmt.Println(zeroCnt)
+	} else {
+		fmt.Println(oneCnt)
+	}
+}
