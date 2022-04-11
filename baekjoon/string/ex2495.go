24beb56c7806e3ee8f0c8050597310e994537406
thisisnew <thisisnew@naver.com>
2022-04-11 20:46:15 +0900

    연속구간

--- baekjoon/string/ex2495.go
+++ baekjoon/string/ex2495.go
@@ -22,18 +22,20 @@ func countContinuityNumbers(input string) {
 
 	var max = 1
 	var cnt = 1
+	var prev = input[0:1]
 
 	for i := 1; i < len(input); i++ {
-		if input[i:i+1] == input[i-1:i] {
+		if input[i:i+1] == prev {
 			cnt++
-			continue
+		} else {
+			cnt = 1
 		}
 
 		if cnt > max {
 			max = cnt
 		}
 
-		cnt = 1
+		prev = input[i : i+1]
 	}
 
 	fmt.Println(max)
