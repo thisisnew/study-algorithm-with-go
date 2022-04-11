d659c6bba767c29af457bf892de996641f896c28
thisisnew <thisisnew@naver.com>
2022-04-11 20:40:32 +0900

    연속구간

--- baekjoon/string/ex2495.go
+++ baekjoon/string/ex2495.go
@@ -9,40 +9,32 @@ import (
 func main() {
 
 	var read = bufio.NewReader(os.Stdin)
-	var max int
+
 	for i := 0; i < 3; i++ {
 		var input string
 		fmt.Fscanln(read, &input)
-		cnt := countContinuityNumbers(input)
-		if max < cnt {
-			max = cnt
-		}
+		countContinuityNumbers(input)
 	}
-
-	fmt.Println(max)
 
 }
 
-func countContinuityNumbers(input string) int {
+func countContinuityNumbers(input string) {
 
-	var out = 0
-
-	var prev string
+	var max = 1
 	var cnt = 1
-	for i := 1; i < len(input); i++ {
-		prev = input[i-1 : i]
 
-		if input[i:i+1] == prev {
+	for i := 1; i < len(input); i++ {
+		if input[i:i+1] == input[i-1:i] {
+			cnt++
 			continue
 		}
 
-		if cnt > out {
-			out = cnt
+		if cnt > max {
+			max = cnt
 		}
 
 		cnt = 1
 	}
 
-	return out
-
+	fmt.Println(max)
 }
