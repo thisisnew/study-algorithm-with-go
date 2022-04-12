2b49ea9d632e4cdc39fec73af210068489779988
thisisnew <thisisnew@naver.com>
2022-04-12 20:15:47 +0900

    NN

---
+++ baekjoon/string/ex11944.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	var n int
+	var m int
+
+	var read = bufio.NewReader(os.Stdin)
+	fmt.Fscanln(read, n, m)
+
+	w := strconv.Itoa(n)
+
+	var b strings.Builder
+
+	for i := 0; i < n; i++ {
+		b.WriteString(w)
+	}
+
+	result := b.String()
+
+	if len(result) > len(result[:m]) {
+		fmt.Println(result[:m])
+	} else {
+		fmt.Println(result)
+	}
+
+}
