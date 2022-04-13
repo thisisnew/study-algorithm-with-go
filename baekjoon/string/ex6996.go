343f24113d4aa5bc08287c56ac6b21ba6d59fb83
thisisnew <thisisnew@naver.com>
2022-04-13 21:50:37 +0900

    애너그램

---
+++ baekjoon/string/ex6996.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strings"
+)
+
+func main() {
+	var read = bufio.NewReader(os.Stdin)
+	var n int
+	fmt.Fscanln(read, &n)
+
+	for i := 0; i < n; i++ {
+		var a, b string
+		var answer strings.Builder
+		fmt.Fscanln(read, &a, &b)
+
+		answer.WriteString(a + " & " + b + " are ")
+
+		aRunes := []rune(a)
+		sort.Slice(aRunes, func(i int, j int) bool { return aRunes[i] < aRunes[j] })
+		bRunes := []rune(b)
+		sort.Slice(bRunes, func(i int, j int) bool { return bRunes[i] < bRunes[j] })
+
+		if string(aRunes) != string(bRunes) {
+			answer.WriteString("NOT ")
+		}
+
+		answer.WriteString("anagrams.")
+
+		fmt.Println(answer.String())
+	}
+
+}
