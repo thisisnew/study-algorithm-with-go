78f221fcdb2bb6b1e5c02c9e25af6e2f47186d41
thisisnew <thisisnew@naver.com>
2022-04-14 20:41:48 +0900

    비밀번호 발음하기

--- baekjoon/string/ex4659.go
+++ baekjoon/string/ex4659.go
@@ -17,19 +17,42 @@ func main() {
 			break
 		}
 
-		prevRn := input[0:1]
+		var vowelCnt int
+		var consCnt int
+		var prevRn = input[0:1]
 
 		for i := 1; i < len(input); i++ {
 			ch := input[i : i+1]
 
-			if ch == "a" || ch == "e" || ch == "i" || ch == "o" || ch == "u" {
-
-			} else {
-
+			switch {
+			case ch == "a", ch == "i", ch == "o", ch == "u", ch == "e":
+				vowelCnt++
+			default:
+				consCnt++
 			}
 
+			if vowelCnt == 3 || consCnt == 3 {
+				fmt.Println(fmt.Sprintf("<%s> is not acceptable.", input))
+				break
+			}
+
+			if prevRn == ch {
+				str := fmt.Sprintf("%s%s", prevRn, ch)
+
+				if str != "ee" && str != "oo" {
+					fmt.Println(fmt.Sprintf("<%s> is not acceptable.", input))
+					break
+				}
+			}
+
+			prevRn = ch
 		}
 
-	}
+		if vowelCnt == 0 {
+			fmt.Println(fmt.Sprintf("<%s> is not acceptable.", input))
+			continue
+		}
 
+		fmt.Println(fmt.Sprintf("<%s> is acceptable.", input))
+	}
 }
