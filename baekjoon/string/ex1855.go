94aa892c3d3b000f4aca0c7431df85111e2e580f
thisisnew <thisisnew@naver.com>
2022-04-19 20:08:43 +0900

    암호

---
+++ baekjoon/string/ex1855.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	var n int
+	var input string
+	var read = bufio.NewReader(os.Stdin)
+	fmt.Fscanln(read, &n)
+	fmt.Fscanln(read, &input)
+
+	var arr = make([][]string, n)
+
+	for i := range arr {
+		arr[i] = make([]string, n)
+	}
+
+	var h = 0
+	var v = 0
+	for i := 0; i < len(input); i++ {
+
+		if i > 0 && i%n == 0 {
+			h++
+			v = 0
+		}
+
+		arr[h][v] = input[v : v+1]
+		v++
+	}
+
+	h = 0
+	v = 0
+	for i := 0; i < len(input); i++ {
+		if i > 0 && i%n == 0 {
+			v++
+			h = 0
+		}
+		fmt.Print(arr[h][v])
+		h++
+	}
+}
