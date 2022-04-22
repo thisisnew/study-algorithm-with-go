512a5e059e06249186f527661dec2b7600c13684
thisisnew <thisisnew@naver.com>
2022-04-22 21:02:23 +0900

    박사 과정

---
+++ baekjoon/string/ex5026.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	var n int
+	var read = bufio.NewReader(os.Stdin)
+	fmt.Fscanln(read, &n)
+
+	for i := 0; i < n; i++ {
+		var input string
+		fmt.Fscanln(read, &input)
+
+		isAddition, a, b := isAddition(input)
+
+		if isAddition {
+			fmt.Println(a + b)
+			continue
+		}
+
+		fmt.Println("skipped")
+
+	}
+}
+
+func isAddition(input string) (bool, int, int) {
+
+	addition := strings.Split(input, "+")
+
+	if len(addition) == 0 {
+		return false, 0, 0
+	}
+
+	a, _ := strconv.Atoi(addition[0])
+	b, _ := strconv.Atoi(addition[1])
+
+	return true, a, b
+}
