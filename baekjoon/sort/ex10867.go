e93f76ce763d090b00e848754054b728da3594ae
thisisnew <thisisnew@naver.com>
2022-04-26 21:06:34 +0900

    중복 빼고 정렬하기

--- baekjoon/sort/ex10867.go
+++ baekjoon/sort/ex10867.go
@@ -9,13 +9,15 @@ import (
 func main() {
 
 	var n int
-	var read = bufio.NewReader(os.Args)
+	var read = bufio.NewReader(os.Stdin)
 	fmt.Fscanln(read, &n)
+
+	var arr []int
 
 	for i := 0; i < n; i++ {
 		var num int
 		fmt.Fscanln(read, &num)
-
+		arr = append(arr, num)
 	}
 
 }
