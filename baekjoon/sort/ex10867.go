4804bc5a90aab7bd88f42478fd732d8c69767187
thisisnew <thisisnew@naver.com>
2022-04-26 21:13:47 +0900

    중복 빼고 정렬하기

--- baekjoon/sort/ex10867.go
+++ baekjoon/sort/ex10867.go
@@ -20,4 +20,20 @@ func main() {
 		arr = append(arr, num)
 	}
 
+	for i := 0; i < len(arr); i++ {
+		var minIndex = i
+
+		for j := i; j < len(arr); j++ {
+			if arr[j] < arr[minIndex] {
+				minIndex = j
+			}
+		}
+
+		arr[i], arr[minIndex] = arr[minIndex], arr[i]
+	}
+
+	for _, item := range arr {
+
+	}
+
 }
