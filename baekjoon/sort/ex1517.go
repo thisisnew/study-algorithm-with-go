7da54488f8466b28c944b39380c001af584dca79
thisisnew <thisisnew@naver.com>
2022-04-28 05:40:59 +0900

    버블소트

---
+++ baekjoon/sort/ex1517.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	var n int
+	var read = bufio.NewReader(os.Stdin)
+	fmt.Fscanln(read, &n)
+
+	text, _, _ := read.ReadLine()
+	nums := strings.Split(string(text), " ")
+
+	var result int
+
+	for i := n; i > 0; i-- {
+
+		isSwap := false
+
+		for j := 0; j < i-1; j++ {
+			if nums[j] > nums[j+1] {
+				nums[j+1], nums[j] = nums[j], nums[j+1]
+				isSwap = true
+				result++
+			}
+		}
+
+		if !isSwap {
+			break
+		}
+	}
+
+	fmt.Println(result)
+}
