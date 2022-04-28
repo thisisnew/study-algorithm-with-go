34454c2dce0dd656b24d815b5518c2ad3fed1679
thisisnew <thisisnew@naver.com>
2022-04-29 06:04:19 +0900

    삽입정렬

---
+++ baekjoon/sort/insertion_sort.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var items = []int{9, 8, 7, 6, 5, 4, 3, 2, 1}
+
+	for i := 1; i < len(items); i++ {
+
+		for j := i; j > 0; j-- {
+			if items[j-1] > items[j] {
+				items[j-1], items[j] = items[j], items[j-1]
+			}
+		}
+	}
+
+	fmt.Println(items)
+}
