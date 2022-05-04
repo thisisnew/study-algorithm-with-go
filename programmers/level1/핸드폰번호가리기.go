66c8e849a0d8279a103727af43d0d964d7b9b94d
thisisnew <thisisnew@naver.com>
2022-05-05 07:22:39 +0900

    핸드폰 번호 가리기

--- programmers/level1/핸드폰번호가리기.go
+++ programmers/level1/핸드폰번호가리기.go
@@ -10,19 +10,5 @@ func main() {
 }
 
 func 핸드폰번호가리기(phoneNumber string) string {
-
-	n := len(phoneNumber) - 4
-
-	return genMasking(n) + phoneNumber[len(phoneNumber)-4:]
-}
-
-func genMasking(n int) string {
-
-	var result strings.Builder
-
-	for i := 0; i < n; i++ {
-		result.WriteString("*")
-	}
-
-	return result.String()
+	return strings.Repeat("*", len(phoneNumber)-4) + phoneNumber[len(phoneNumber)-4:]
 }
