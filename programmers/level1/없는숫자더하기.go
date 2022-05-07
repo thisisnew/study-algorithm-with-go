c49a12281353fe306c98b67087f03c12af03e5c3
thisisnew <thisisnew@naver.com>
2022-05-07 20:25:07 +0900

    없는 숫자 더하기

--- programmers/level1/없는숫자더하기.go
+++ programmers/level1/없는숫자더하기.go
@@ -2,6 +2,8 @@ package main
 
 import "fmt"
 
+const totalZeroToNine = 45
+
 func main() {
 	n := []int{1, 2, 3, 4, 6, 7, 8, 0}
 	fmt.Println(없는숫자더하기(n))
@@ -14,5 +16,5 @@ func 없는숫자더하기(numbers []int) int {
 		result += n
 	}
 
-	return 45 - result
+	return totalZeroToNine - result
 }
