8646a0353f2ac5d72982fbb6616abb638cdcb538
thisisnew <thisisnew@naver.com>
2022-05-09 06:33:14 +0900

    정수 제곱근 판별

--- programmers/level1/정수제곱근판별.go
+++ programmers/level1/정수제곱근판별.go
@@ -8,48 +8,30 @@ func main() {
 
 func 정수제곱근판별(n int64) int64 {
 
-	var start int64 = 1
 	var result int64
+	var num int64
 
-	if n > 100 {
-		start = n / 10
+	for {
 
-		for {
-			pow := start * start
+		num++
 
-			if pow == n {
-				result = start + 1
-				return result * result
-			}
-
-			if pow < n {
-				break
-			}
-
-			if pow > n {
-				start = start / 10
-				continue
-			}
-
-		}
-	}
-
-	for i := start; i < n; i++ {
-		pow := i * i
+		pow := num * num
 
 		if pow < n {
 			continue
 		}
 
 		if pow > n {
-			return -1
+			result = -1
+			break
 		}
 
 		if pow == n {
-			result = i + 1
-			return result * result
+			result = (num + 1) * (num + 1)
+			break
 		}
+
 	}
 
-	return -1
+	return result
 }
