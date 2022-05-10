90c40afd5f88a4de99e6488cfa4de917f8aaed6f
thisisnew <thisisnew@naver.com>
2022-05-11 06:22:26 +0900

    소수 만들기 (진행 중)

---
+++ programmers/level1/소수만들기.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+func main() {
+	nums := []int{1, 2, 3, 4}
+
+	fmt.Println(소수만들기(nums))
+}
+
+func 소수만들기(nums []int) int {
+	var result int
+
+	for i := 0; i < len(nums)-2; i++ {
+
+		n := nums[i] + nums[i+1] + nums[i+2]
+
+		if !isPrimeNumber(n) {
+			continue
+		}
+
+		result++
+	}
+
+	return result
+}
+
+func isPrimeNumber(n int) bool {
+
+	for i := 2; i < n; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+
+	return true
+}
