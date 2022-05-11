70c59b7114b786b32cbd855f84aef2b1d50ea53d
thisisnew <thisisnew@naver.com>
2022-05-12 07:31:45 +0900

    피보나치 수

---
+++ programmers/level2/피보나치수.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(피보나치수(3))
+}
+
+func 피보나치수(n int) int {
+
+	var result = fibo(n)
+
+	return result % 1234567
+}
+
+func fibo(n int) int {
+
+	if n <= 1 {
+		return n
+	}
+
+	return fibo(n-1) + fibo(n-2)
+}
