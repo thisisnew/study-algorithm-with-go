83cc3630de6d6a746adced78adb68fd42e72065d
thisisnew <thisisnew@naver.com>
2022-05-12 21:43:55 +0900

    가장 큰 수

---
+++ programmers/level2/가장큰수.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	numbers := []int{3, 30, 34, 5, 9}
+
+	fmt.Println(가장큰수(numbers))
+}
+
+func 가장큰수(numbers []int) string {
+
+	digit := 10
+
+	sort.Slice(numbers, func(i, j int) bool {
+
+		ni := getSingleDigit(numbers[i], digit)
+		nj := getSingleDigit(numbers[j], digit)
+
+		for {
+			if ni != nj {
+				break
+			}
+
+			digit = digit * 10
+			ni = getSingleDigit(numbers[i], digit)
+			nj = getSingleDigit(numbers[j], digit)
+		}
+
+		return ni > nj
+	})
+
+	var result strings.Builder
+
+	for _, n := range numbers {
+		result.WriteString(strconv.Itoa(n))
+	}
+
+	return result.String()
+}
+
+func getSingleDigit(n int, digit int) int {
+
+	if n*digit < digit {
+		return n
+	}
+
+	for {
+		if n%digit < digit {
+			return n / digit
+		}
+
+		n = n / digit
+	}
+}
