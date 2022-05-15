f4ec3dc29f6ac6d9a69e42044fad9e3b1c713478
thisisnew <thisisnew@naver.com>
2022-05-15 20:38:03 +0900

    소수 만들기

--- programmers/level1/소수만들기.go
+++ programmers/level1/소수만들기.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	nums := []int{
-		1, 2, 7, 6, 4,
+		1, 2, 3, 4,
 	}
 
 	fmt.Println(소수만들기(nums))
@@ -14,7 +14,6 @@ func 소수만들기(nums []int) int {
 	var result int
 
 	for i := 0; i < len(nums)-2; i++ {
-
 		for j := i + 1; j < len(nums)-1; j++ {
 			for k := j + 1; k < len(nums); k++ {
 				n := nums[i] + nums[j] + nums[k]
