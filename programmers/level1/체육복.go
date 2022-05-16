e3ce31133989f3c68c75b76b4e4adc944e3547c6
thisisnew <thisisnew@naver.com>
2022-05-17 05:35:45 +0900

    체육복

--- programmers/level1/체육복.go
+++ programmers/level1/체육복.go
@@ -5,12 +5,6 @@ import (
 	"sort"
 )
 
-type Students struct {
-	num     int
-	lost    bool
-	reserve bool
-}
-
 func main() {
 
 	n := 3
@@ -30,67 +24,29 @@ func 체육복(n int, lost []int, reserve []int) int {
 		return reserve[i] < reserve[j]
 	})
 
-	var lostAndNotReserved []int
-	var lostAndNotBorrowed []int
+	var count int
 
 	for i := 0; i < len(lost); i++ {
-
-		isReserved, reservedButNotLost := isReservedStudent(reserve, lost[i])
-
-		reserve = reservedButNotLost
-
-		if isReserved {
-			continue
+		for j := 0; j < len(reserve); j++ {
+			if lost[i] == reserve[j] {
+				count++
+				lost[i] = -1
+				reserve[j] = -1
+				break
+			}
 		}
-
-		lostAndNotReserved = append(lostAndNotReserved, lost[i])
 	}
 
-	for i := 0; i < len(lostAndNotReserved); i++ {
-		canBorrow, notReservedYet := canBorrowGymSuit(reserve, lostAndNotReserved[i])
-
-		reserve = notReservedYet
-
-		if canBorrow {
-			continue
+	for i := 0; i < len(lost); i++ {
+		for j := 0; j < len(reserve); j++ {
+			if lost[i] == reserve[j]+1 || lost[i] == reserve[j]-1 {
+				count++
+				reserve[j] = -1
+				break
+			}
 		}
-
-		lostAndNotBorrowed = append(lostAndNotBorrowed, lostAndNotReserved[i])
 	}
 
-	return n - len(lostAndNotBorrowed)
-}
+	return n - len(lost) + count
 
-func isReservedStudent(reserve []int, n int) (bool, []int) {
-
-	var reservedButNotLost []int
-	var isReserved bool
-
-	for _, rn := range reserve {
-		if rn == n {
-			isReserved = true
-			continue
-		}
-
-		reservedButNotLost = append(reservedButNotLost, rn)
-	}
-
-	return isReserved, reservedButNotLost
-}
-
-func canBorrowGymSuit(reserve []int, n int) (bool, []int) {
-
-	var notLendYet []int
-	var canBorrow bool
-
-	for _, rn := range reserve {
-		if rn == n-1 || rn == n+1 {
-			canBorrow = true
-			continue
-		}
-
-		notLendYet = append(notLendYet, rn)
-	}
-
-	return canBorrow, notLendYet
 }
