d3ba30a8dfadacf36d587404e1cfb22e45bf143d
thisisnew <thisisnew@naver.com>
2022-05-19 05:37:18 +0900

    조이스틱

--- programmers/level2/조이스틱.go
+++ programmers/level2/조이스틱.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/big"
+)
 
 func main() {
 	fmt.Println(조이스틱("JEROEN"))
@@ -9,68 +12,153 @@ func main() {
 func 조이스틱(name string) int {
 	var result int
 	var idx int
+	var prevIdx []int
+	var isLast bool
 
-	for _, rn := range name {
+	for {
 
-		upCnt, upIdx := controlUp(idx, rn-65)
-		downCnt, downIdx := controlDown(idx, rn-65)
-
-		if upCnt > downCnt {
-			result += downCnt
-			idx = downIdx
-		} else {
-			result += upCnt
-			idx = upIdx
+		if isLast {
+			break
 		}
 
+		token := name[idx : idx+1]
+		tokenToInt := int(big.NewInt(0).SetBytes([]byte(token)).Uint64()) - 65
+
+		upCnt := controlUp(tokenToInt)
+		downCnt := controlDown(tokenToInt)
+
+		if upCnt <= downCnt {
+			result += upCnt
+		} else {
+			result += downCnt
+		}
+
+		prevIdx = append(prevIdx, idx)
+		lCnt, lIdx := cursorLeft(idx, prevIdx, name)
+		rCnt, rIdx := cursorRight(idx, prevIdx, name)
+
+		isLast = isLastIdx(idx, len(name)-1)
+
+		if rCnt <= lCnt {
+			result += rCnt
+			idx = rIdx
+		} else {
+			result += lCnt
+			idx = lIdx
+		}
 	}
 
 	return result
 }
 
-func controlUp(idx int, dist int32) (int, int) {
+func isLastIdx(idx, ln int) bool {
+	if idx == ln {
+		return true
+	}
+
+	return false
+}
+
+func controlUp(dist int) int {
+
+	var result int
+	var i int
+
+	for {
+		if i == dist {
+			break
+		}
+
+		result++
+		i++
+	}
+
+	return result
+}
+
+func controlDown(dist int) int {
+
+	var result int
+	var i int
+
+	for {
+		if i == dist {
+			break
+		}
+
+		result++
+		i--
+
+		if i < 0 {
+			i = 25
+		}
+	}
+
+	return result
+}
+
+func cursorLeft(idx int, prevIdx []int, name string) (int, int) {
+
+	var result int
+	var i = idx
+
+	for {
+
+		result++
+		i--
+
+		if i < 0 {
+			i = len(name) - 1
+		}
+
+		if i == idx {
+			break
+		}
+
+		if name[i:i+1] == "A" {
+			continue
+		}
+
+		if isInPrevIdx(i, prevIdx) {
+			continue
+		}
+
+		break
+	}
+
+	return result, i
+}
+
+func cursorRight(idx int, prevIdx []int, name string) (int, int) {
 
 	var result int
 
-	for {
-		if int32(idx) == dist {
-			break
-		}
+	for i := idx + 1; i < len(name); i++ {
 
 		result++
 		idx++
 
-		if idx > 26 {
-			idx = 0
+		if name[i:i+1] == "A" {
+			continue
 		}
+
+		if isInPrevIdx(idx, prevIdx) {
+			continue
+		}
+
+		return result, idx
 	}
 
 	return result, idx
 }
 
-func controlDown(idx int, dist int32) (int, int) {
-	var result int
+func isInPrevIdx(idx int, prevIdx []int) bool {
 
-	for {
-		if int32(idx) == dist {
-			break
-		}
-
-		result++
-		idx--
-
-		if idx < 0 {
-			idx = 26
+	for _, i := range prevIdx {
+		if idx == i {
+			return true
 		}
 	}
 
-	return result, idx
-}
-
-func cursorLeft() {
-
-}
-
-func cursorRight() {
-
+	return false
 }
