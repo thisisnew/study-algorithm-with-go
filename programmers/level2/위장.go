ebc48462f90b0fba9957678275174f5397a56b81
thisisnew <thisisnew@naver.com>
2022-05-20 05:26:19 +0900

    위장

---
+++ programmers/level2/위장.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	clothes := [][]string{
+		{"yellowhat", "headgear"},
+		{"bluesunglasses", "eyewear"},
+		{"green_turban", "headgear"},
+	}
+
+	fmt.Println(위장(clothes))
+}
+
+func 위장(clothes [][]string) int {
+	return 0
+}
