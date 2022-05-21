5bba4899196ae821dc7ddfb2208153c75b55a2eb
thisisnew <thisisnew@naver.com>
2022-05-22 00:04:12 +0900

    주차요금계산

--- programmers/level2/주차요금계산.go
+++ programmers/level2/주차요금계산.go
@@ -2,28 +2,46 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strings"
 	"time"
 )
 
 func main() {
-	fees := []int{180, 5000, 10, 600}
-	result := []string{"05:34 5961 IN", "06:00 0000 IN", "06:34 0000 OUT", "07:59 5961 OUT", "07:59 0148 IN", "18:59 0000 IN", "19:09 0148 OUT", "22:59 5961 IN", "23:00 5961 OUT"}
+	fees := []int{
+		180,
+		5000,
+		10,
+		600,
+	}
+
+	result := []string{
+		"05:34 5961 IN",
+		"06:00 0000 IN",
+		"06:34 0000 OUT",
+		"07:59 5961 OUT",
+		"07:59 0148 IN",
+		"18:59 0000 IN",
+		"19:09 0148 OUT",
+		"22:59 5961 IN",
+		"23:00 5961 OUT",
+	}
 
 	fmt.Println(주차요금계산(fees, result))
 }
 
-func 주차요금계산(fees []int, records []string) []int {
+func 주차요금계산(fees []int, records []string) []float64 {
 
 	vehicles := calculateTimesByVehicles(records)
 
-	return calculationFeesByVehicles(fees, vehicles)
+	return calculateFeesByVehicles(fees, vehicles)
+
 }
 
-func calculateTimesByVehicles(records []string) map[string]int {
+func calculateTimesByVehicles(records []string) map[string]float64 {
 
 	var temp = map[string][]string{}
-	var result = map[string]int{}
+	var result = map[string]float64{}
 
 	for _, record := range records {
 
@@ -61,33 +79,35 @@ func calculateTimesByVehicles(records []string) map[string]int {
 	return result
 }
 
-func getDurationTwoTimes(after, before string) int {
+func getDurationTwoTimes(after, before string) float64 {
 
 	layout := "15:04"
 	a, _ := time.Parse(layout, after)
 	b, _ := time.Parse(layout, before)
 
-	return int(a.Sub(b).Minutes())
+	return a.Sub(b).Minutes()
 }
 
-func calculationFeesByVehicles(fees []int, vehicles map[string]int) []int {
+func calculateFeesByVehicles(fees []int, vehicles map[string]float64) []float64 {
 
-	baseMinute := fees[0]
-	baseFee := fees[1]
-	unitMinute := fees[2]
-	unitFee := fees[3]
+	baseMinute := float64(fees[0])
+	baseFee := float64(fees[1])
+	unitMinute := float64(fees[2])
+	unitFee := float64(fees[3])
 
-	var result []int
+	var result []float64
 
 	for _, vh := range vehicles {
 
 		if vh <= baseMinute {
-			result = append(result, baseFee)
+			result = append(result, float64(baseFee))
 
 			continue
 		}
 
-		sum := baseFee + ((vh - baseMinute) / unitMinute * unitFee)
+		fee := math.Ceil((vh - baseMinute) / unitMinute)
+
+		sum := baseFee + fee*unitFee
 		result = append(result, sum)
 	}
 
