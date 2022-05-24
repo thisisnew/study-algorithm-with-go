b6360889332818c53011c1f70256126588a291d2
thisisnew <thisisnew@naver.com>
2022-05-25 05:45:51 +0900

    h-index

--- programmers/level2/H-Index.go
+++ programmers/level2/H-Index.go
@@ -15,71 +15,20 @@ func main() {
 
 func hIndex(citations []int) int {
 
-	var result, a, b int
+	var result int
 
 	sort.Slice(citations, func(i, j int) bool {
-		return citations[i] > citations[j]
+		return citations[i] < citations[j]
 	})
 
-	for {
-
-		if citations[0] <= result {
-			result = len(citations)
-			break
-		}
-
-		a = above(result, citations)
-		b = below(result, citations)
-
-		if a != result || b != result {
-			result++
+	for i, c := range citations {
+		if c < len(citations)-i {
 			continue
 		}
 
+		result = len(citations) - i
 		break
 	}
 
 	return result
-}
-
-func above(n int, citations []int) int {
-
-	var result = 0
-
-	for _, c := range citations {
-
-		if c < n {
-			continue
-		}
-
-		result++
-
-		if result > n {
-			break
-		}
-	}
-
-	return result
-
-}
-
-func below(n int, citations []int) int {
-
-	var result = 0
-
-	for _, c := range citations {
-
-		if c > n {
-			continue
-		}
-
-		result++
-
-		if result > n {
-			break
-		}
-	}
-
-	return result
-
 }
