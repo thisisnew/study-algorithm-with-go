bba7504a8ad5ed1326144951a032eb1cc9708a9a
thisisnew <thisisnew@naver.com>
2022-05-26 06:02:16 +0900

    다리를지나는트럭

--- programmers/level2/다리를지나는트럭.go
+++ programmers/level2/다리를지나는트럭.go
@@ -1,9 +1,28 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	bridge_length := 2
+	weight := 10
+	truck_weights := []int{7, 4, 5, 6}
+
+	fmt.Println(다리를지나는트럭(bridge_length, weight, truck_weights))
 
 }
 
 func 다리를지나는트럭(bridge_length int, weight int, truck_weights []int) int {
-	return 0
+
+	var result int
+
+	return result
+}
+
+func crossingTheBridge() {
+
+}
+
+func waitingTrucks() {
+
 }
