aea4f91b3a44a4b839ea79c0e98e8779aa560a1d
thisisnew <thisisnew@naver.com>
2022-05-31 06:06:57 +0900

    두개 이하로 다른 비트

--- programmers/level2/2개이하로다른비트.go
+++ programmers/level2/2개이하로다른비트.go
@@ -8,10 +8,12 @@ func main() {
 
 func 두개이하로다른비트(numbers []int64) []int64 {
 
-	var result []int64
+	var result = make([]int64, len(numbers))
 
-	for _, n := range numbers {
+	for i, n := range numbers {
+		minimalN := getMinimalNumberHasDifBits(n)
 
+		result[i] = minimalN
 	}
 
 	return result
@@ -19,11 +21,18 @@ func 두개이하로다른비트(numbers []int64) []int64 {
 
 func getMinimalNumberHasDifBits(n int64) int64 {
 
-	var result int64
+	var result = n
 	binN := strconv.FormatInt(n, 2)
 
 	for {
 
+		binDif := strconv.FormatInt(result, 2)
+
+		if countDifBits(binN, binDif) <= 2 {
+			break
+		}
+
+		result++
 	}
 
 	return result
@@ -35,6 +44,10 @@ func countDifBits(binN, binDif string) int {
 	var result int
 
 	for i := 0; i < len(binDif); i++ {
+
+		if binDif[i:i+1] != binN[i:i+1] {
+			result++
+		}
 
 	}
 
