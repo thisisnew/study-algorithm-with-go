ebaa261c8826d1c95225c2ffe63f8f4b69364c35
thisisnew <thisisnew@naver.com>
2022-05-31 06:03:32 +0900

    두개 이하로 다른 비트

---
+++ programmers/level2/2개이하로다른비트.go
@@ -0,0 +1,43 @@
+package main
+
+import "strconv"
+
+func main() {
+
+}
+
+func 두개이하로다른비트(numbers []int64) []int64 {
+
+	var result []int64
+
+	for _, n := range numbers {
+
+	}
+
+	return result
+}
+
+func getMinimalNumberHasDifBits(n int64) int64 {
+
+	var result int64
+	binN := strconv.FormatInt(n, 2)
+
+	for {
+
+	}
+
+	return result
+
+}
+
+func countDifBits(binN, binDif string) int {
+
+	var result int
+
+	for i := 0; i < len(binDif); i++ {
+
+	}
+
+	return result
+
+}
