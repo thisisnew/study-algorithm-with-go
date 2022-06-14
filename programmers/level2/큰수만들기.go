3dca9fa4ebeed2e88d3d0a85f04a43028db17c56
thisisnew <thisisnew@naver.com>
2022-06-15 06:15:34 +0900

    큰수만들기

--- programmers/level2/큰수만들기.go
+++ programmers/level2/큰수만들기.go
@@ -1,9 +1,28 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
 
+func main() {
+	fmt.Println(큰수만들기("1231234", 3))
 }
 
 func 큰수만들기(number string, k int) string {
-	return ""
+
+	var sl []string
+
+	for _, n := range number {
+		sl = append(sl, string(n))
+	}
+
+	sort.Slice(sl, func(i, j int) bool {
+		return sl[i] > sl[j]
+	})
+
+	result := strings.Join(sl, "")
+
+	return result[0 : len(result)-k]
 }
