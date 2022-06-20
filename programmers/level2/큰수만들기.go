ffe27089180b9cc58690fb6b254117abc6df89c0
thisisnew <thisisnew@naver.com>
2022-06-21 06:11:08 +0900

    큰 수 만들기

--- programmers/level2/큰수만들기.go
+++ programmers/level2/큰수만들기.go
@@ -12,29 +12,10 @@ func main() {
 
 func 큰수만들기(number string, k int) string {
 
-	/*class Solution {
-		public String solution(String number, int k) {
-		StringBuilder sb = new StringBuilder();
-		int index = 0;
-		int max = 0;
-		for(int i=0; i<number.length() - k; i++) {
-		max = 0;
-		for(int j = index; j<= k+i; j++) {
-		if(max < number.charAt(j)-'0') {
-		max = number.charAt(j)-'0';
-		index = j+1;
-	}
-	}
-		sb.append(max);
-	}
-		return sb.toString();
-	}
-	}*/
-
 	var result strings.Builder
 	var idx = 0
 
-	for i := 0; i < len([]rune(number)); i++ {
+	for i := 0; i < len([]rune(number))-k; i++ {
 		var max = 0
 		for j := idx; j <= k+1; j++ {
 			num, _ := strconv.Atoi(number[j : j+1])
