054411f7e549efb9701aa9a9437432cb8f1e89eb
thisisnew <thisisnew@naver.com>
2022-06-24 08:21:11 +0900

    k진수에서 소수 개수 구하기

--- programmers/level2/k진수에서소수개수구하기.go
+++ programmers/level2/k진수에서소수개수구하기.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 func main() {
@@ -15,10 +16,19 @@ func k진수에서소수개수구하기(n int, k int) int {
 
 	kString := convertDecimalToKNumber(n, k)
 
-	kNumber, _ := strconv.Atoi(kString)
+	//kNumber, _ := strconv.Atoi(kString)
+
+	var sb strings.Builder
 
 	for _, num := range kString {
 
+		if string(num) == "0" {
+			result += addResultIfPrimeNumber(sb.String())
+			sb.Reset()
+			continue
+		}
+
+		sb.WriteString(string(num))
 	}
 
 	return result
@@ -26,4 +36,19 @@ func k진수에서소수개수구하기(n int, k int) int {
 
 func convertDecimalToKNumber(n int, k int) string {
 	return strconv.FormatInt(int64(n), k)
+}
+
+func addResultIfPrimeNumber(num string) int {
+
+	n, _ := strconv.Atoi(num)
+
+	for i := 2; i <= n; i++ {
+
+		if n%i == 0 {
+			return 0
+		}
+
+	}
+
+	return 1
 }
