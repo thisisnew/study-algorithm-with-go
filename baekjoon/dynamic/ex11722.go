c05d839dfbb1df7d69027e03e805d40e354585af
thisisnew <thisisnew@naver.com>
2022-06-29 05:53:34 +0900

    가장 긴 감소하는 부분 수열

---
+++ baekjoon/dynamic/ex11722.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	var n int
+	var read = bufio.NewReader(os.Stdin)
+	fmt.Fscanln(read, &n)
+
+	text, _, _ := read.ReadLine()
+
+	sl := strings.Split(string(text), " ")
+
+	var result []string
+
+	for _, s := range sl {
+
+	}
+
+}
