393566eadcf08c22570c4cd4f9f56854b3abc275
thisisnew <thisisnew@naver.com>
2022-07-04 06:15:09 +0900

    n^2 배열 자르기

---
+++ programmers/level2/n^2배열자르기.go
@@ -0,0 +1,35 @@
+package main
+
+func main() {
+
+}
+
+func n2배열자르기(n int, left int64, right int64) []int {
+
+	var sl = generate2DSlice(n)
+
+	var result = convert2DSliceTo1DSlice(sl)
+
+	return result
+}
+
+func generate2DSlice(n int) [][]int {
+
+	var result = make([][]int, n, n)
+
+	for i := 0; i < n; i++ {
+		for j := 0; j < n; j++ {
+			result[i][j] = n - 1
+		}
+	}
+
+	return result
+}
+
+func convert2DSliceTo1DSlice(sl [][]int) []int {
+
+	var result []int
+
+	return result
+
+}
