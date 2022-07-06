1887a41b60fa603084b242eecd0eb766a8e968de
thisisnew <thisisnew@naver.com>
2022-07-07 06:14:10 +0900

    n^2 배열 자르기

--- programmers/level2/n^2배열자르기.go
+++ programmers/level2/n^2배열자르기.go
@@ -7,18 +7,22 @@ func main() {
 }
 
 func n2배열자르기(n int, left int64, right int64) []int {
-	return getSliceFromLeftToRight(convert2DSliceTo1DSlice(generate2DSlice(n)), left, right)
+	return convert2DSliceTo1DSlice(n, left, right)
 }
 
-func generate2DSlice(n int) [][]int {
+func convert2DSliceTo1DSlice(n int, left int64, right int64) []int {
 
-	var result = make([][]int, n, n)
+	var sl []int
+	var result []int
 
 	for i := 0; i < n; i++ {
-		result[i] = make([]int, n)
-
 		for j := 0; j < n; j++ {
-			result[i][j] = getValueCompareIAndJ(i, j)
+			v := getValueCompareIAndJ(i, j)
+			sl = append(sl, v)
+
+			if len(sl) >= (int(left)+1) && len(sl) <= (int(right)+1) {
+				result = append(result, v)
+			}
 		}
 	}
 
@@ -32,28 +36,4 @@ func getValueCompareIAndJ(i, j int) int {
 	}
 
 	return i + 1
-}
-
-func convert2DSliceTo1DSlice(sl [][]int) []int {
-
-	var result []int
-
-	for i := 0; i < len(sl); i++ {
-		for j := 0; j < len(sl); j++ {
-			result = append(result, sl[i][j])
-		}
-	}
-
-	return result
-}
-
-func getSliceFromLeftToRight(sl []int, left int64, right int64) []int {
-
-	var result []int
-
-	for i := left; i <= right; i++ {
-		result = append(result, sl[i])
-	}
-
-	return result
 }
