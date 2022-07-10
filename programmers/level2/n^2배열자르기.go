857a5e2d18dcb6035f942e580383833b9ae0ed39
thisisnew <thisisnew@naver.com>
2022-07-10 14:26:23 +0900

    n^2 배열 자르기

--- programmers/level2/n^2배열자르기.go
+++ programmers/level2/n^2배열자르기.go
@@ -1,42 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	fmt.Println(n2배열자르기(3, 2, 5))
 }
 
-func n2배열자르기(n int, left int64, right int64) []int {
+func n2배열자르기(n int, left int64, right int64) []float64 {
 	return convert2DSliceTo1DSlice(n, left, right)
 }
 
-func convert2DSliceTo1DSlice(n int, left int64, right int64) []int {
+func convert2DSliceTo1DSlice(n int, left int64, right int64) []float64 {
 
-	var sl []int
-	var result []int
+	var result []float64
 
-	for i := 0; i < n; i++ {
-		for j := 0; j < n; j++ {
-			v := getValueCompareIAndJ(i, j)
-			sl = append(sl, v)
-			if len(sl) >= (int(left)+1) && len(sl) <= (int(right)+1) {
-				result = append(result, v)
-			}
-
-			if len(result) >= int(right)-int(left)+1 {
-				break
-			}
-		}
+	for left <= right {
+		result = append(result, math.Max(math.Floor(float64(int(left)/n)), float64(int(left+1)%n)+1))
+		left++
 	}
 
 	return result
-}
-
-func getValueCompareIAndJ(i, j int) int {
-
-	if j > i {
-		return j + 1
-	}
-
-	return i + 1
 }
