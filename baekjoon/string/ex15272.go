1ee0b1c678f8c725ab3eac88e7cf54a7212e2aff
thisisnew <thisisnew@naver.com>
2022-07-20 06:22:30 +0900

    Hissing Microphone

---
+++ baekjoon/string/ex15272.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	var read = bufio.NewReader(os.Stdin)
+	var input string
+	fmt.Fscanln(read, &input)
+
+	if isHiss(input) {
+		fmt.Println("hiss")
+	} else {
+		fmt.Println("no hiss")
+	}
+}
+
+func isHiss(input string) bool {
+
+	var sCnt = 0
+
+	for i := 0; i < len([]rune(input)); i++ {
+		s := input[i : i+1]
+
+		if s == "s" {
+			sCnt++
+		} else {
+			sCnt = 0
+		}
+
+		if sCnt == 2 {
+			return true
+		}
+
+	}
+
+	return false
+}
