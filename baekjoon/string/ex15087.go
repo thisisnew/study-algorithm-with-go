06626d81b5a8a167e5520790b868df47f69b23d6
thisisnew <thisisnew@naver.com>
2022-07-24 21:48:53 +0900

    DRM

--- baekjoon/string/ex15087.go
+++ baekjoon/string/ex15087.go
@@ -15,13 +15,15 @@ func main() {
 
 	pre, post := divideDrm(drm)
 
-	nPre := getNewDrmToken(pre, sumTokenAsciiValue(pre))
-	nPost := getNewDrmToken(post, sumTokenAsciiValue(post))
+	//nPre := getNewDrmToken(pre, sumTokenAsciiValue(pre))
+	//nPost := getNewDrmToken(post, sumTokenAsciiValue(post))
 
+	getNewDrmToken(pre, sumTokenAsciiValue(pre))
+	getNewDrmToken(post, sumTokenAsciiValue(post))
 }
 
 func divideDrm(drm string) (string, string) {
-	return drm[:len([]rune(drm))/2+1], drm[len([]rune(drm))/2+1:]
+	return drm[:len([]rune(drm))/2], drm[len([]rune(drm))/2:]
 }
 
 func sumTokenAsciiValue(input string) int {
@@ -29,7 +31,7 @@ func sumTokenAsciiValue(input string) int {
 	var result int
 
 	for _, n := range input {
-		result += int(n)
+		result += getDrmIntValues(string(n))
 	}
 
 	return result
@@ -41,10 +43,10 @@ func getNewDrmToken(old string, asciiValue int) string {
 
 	for _, o := range old {
 
-		v := int(o) + asciiValue
+		v := getDrmIntValues(string(o)) + asciiValue
 
-		if v > int('Z') {
-			v = v - int('A')
+		if v > 25 {
+			v = v - 25
 		}
 
 		result.WriteString(strconv.Itoa(v))
@@ -53,6 +55,66 @@ func getNewDrmToken(old string, asciiValue int) string {
 	return result.String()
 }
 
-func getNewDrmWord(old string, token string) string {
+func getDrmIntValues(d string) int {
 
+	switch d {
+	case "A":
+		return 0
+	case "B":
+		return 1
+	case "C":
+		return 2
+	case "D":
+		return 3
+	case "E":
+		return 4
+	case "F":
+		return 5
+	case "G":
+		return 6
+	case "H":
+		return 7
+	case "I":
+		return 8
+	case "J":
+		return 9
+	case "K":
+		return 10
+	case "L":
+		return 11
+	case "M":
+		return 12
+	case "N":
+		return 13
+	case "O":
+		return 14
+	case "P":
+		return 15
+	case "Q":
+		return 16
+	case "R":
+		return 17
+	case "S":
+		return 18
+	case "T":
+		return 19
+	case "U":
+		return 20
+	case "V":
+		return 21
+	case "W":
+		return 22
+	case "X":
+		return 23
+	case "Y":
+		return 24
+	case "Z":
+		return 25
+	}
+
+	return 0
 }
+
+//func getNewDrmWord(old string, token string) string {
+//
+//}
