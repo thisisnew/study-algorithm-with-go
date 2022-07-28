b688dfe28cb78e5222d40535b682b2b721149e64
thisisnew <thisisnew@naver.com>
2022-07-29 00:54:02 +0900

    Counting Sheep

---
+++ baekjoon/string/ex7366.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	var n int
+	var read = bufio.NewReader(os.Stdin)
+
+	fmt.Fscan(read, &n)
+
+	for i := 0; i < n; i++ {
+		var m int
+		fmt.Fscan(read, &m)
+		text, _, _ := read.ReadLine()
+
+		var result int
+
+		for _, word := range strings.Split(string(text), " ") {
+			if isSheep(word) {
+				result++
+			}
+		}
+
+		fmt.Println(result)
+	}
+
+}
+
+func isSheep(word string) bool {
+
+	if word == "sheep" {
+		return true
+	}
+
+	return false
+}
