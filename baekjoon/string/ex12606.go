a622a9cf6ae0c8c6c3eadbd6bd6f0d512c98ebba
thisisnew <thisisnew@naver.com>
2022-08-01 00:30:27 +0900

    Reverse Words (Large)

---
+++ baekjoon/string/ex12606.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	var n int
+	var read = bufio.NewReader(os.Stdin)
+	fmt.Fscanln(read, &n)
+
+	for i := 0; i < n; i++ {
+		text, _, _ := read.ReadLine()
+
+		sl := strings.Split(string(text), " ")
+
+		var result = make([]string, len(sl))
+		for j := len(sl) - 1; j >= 0; j-- {
+			result[len(sl)-1-j] = sl[j]
+		}
+
+		fmt.Println(fmt.Sprintf("Case #%v: %s", i+1, strings.Join(result, " ")))
+	}
+
+}
