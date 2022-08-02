f8b514ed5b0686982c9d40cef039fa628afffa98
thisisnew <thisisnew@naver.com>
2022-08-02 13:38:06 +0900

    비밀 편지

---
+++ baekjoon/string/ex5426.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	var n int
+	var read = bufio.NewReader(os.Stdin)
+	fmt.Fscanln(read, &n)
+
+	for i := 0; i < n; i++ {
+		var input string
+		fmt.Fscanln(read, &input)
+
+		var sq = math.Sqrt(float64(len([]rune(input))))
+
+		var sl = make([][]string, int(sq), int(sq))
+		var idx = 0
+
+		for j := 0; j < len(sl); j++ {
+			sl[j] = make([]string, int(sq))
+
+			for k := 0; k < len(sl[j]); k++ {
+				sl[j][k] = input[idx : idx+1]
+				idx++
+			}
+		}
+
+		var result strings.Builder
+		for j := len(sl) - 1; j >= 0; j-- {
+			for k := 0; k < len(sl); k++ {
+				s := sl[j][k]
+				result.WriteString(s)
+			}
+		}
+
+		fmt.Println(result.String())
+	}
+
+}
