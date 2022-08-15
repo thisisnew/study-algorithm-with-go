4565efc66882a5af5db67c3870a52f8241d9dacb
thisisnew <thisisnew@naver.com>
2022-08-15 19:32:46 +0900

    엠비티아이

--- baekjoon/string/ex24725.go
+++ baekjoon/string/ex24725.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 )
 
+var mbtis = []string{"ISTJ", "ISTP", "ISFJ", "ISFP", "INTJ", "INTP", "INFJ", "INFP", "ESTJ", "ESTP", "ESFJ", "ESFP", "ENTJ", "ENTP", "ENFJ", "ENFP"}
+
 func main() {
 
 	var n, m int
@@ -28,30 +30,59 @@ func main() {
 		sl[i] = props
 	}
 
-	//fmt.Println(sl)
-
 	var result int
-	result += readHorizontalWord(sl)
 
+	//가로
+	if m > 3 {
+		result += readWordHorizontally(sl)
+	}
+
+	//세로
+	if n > 3 {
+		result += readWordVertically(sl, m)
+	}
+
+	if n > 3 && m > 3 {
+		//대각선
+
+	}
 }
 
 func countMBTIs(word string) int {
 
 	var result int
 
-	for _, mbti := range []string{"ISTJ", "ISTP", "ISFJ", "ISFP", "INTJ", "INTP", "INFJ", "INFP", "ESTJ", "ESTP", "ESFJ", "ESFP", "ENTJ", "ENTP", "ENFJ", "ENFP"} {
+	for _, mbti := range mbtis {
 		result += strings.Count(word, mbti)
 	}
 
 	return result
 }
 
-func readHorizontalWord(sl [][]string) int {
+func readWordHorizontally(sl [][]string) int {
 
 	var result int
 
 	for _, props := range sl {
 		result += countMBTIs(strings.Join(props, ""))
+	}
+
+	return result
+}
+
+func readWordVertically(sl [][]string, m int) int {
+
+	var result int
+
+	for i := 0; i < m; i++ {
+
+		var word strings.Builder
+
+		for _, props := range sl {
+			word.WriteString(props[i])
+		}
+
+		result += countMBTIs(word.String())
 	}
 
 	return result
