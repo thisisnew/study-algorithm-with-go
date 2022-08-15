0f155a8381031088a35b926d7b12855e8b7c5337
thisisnew <thisisnew@naver.com>
2022-08-15 19:41:15 +0900

    엠비티아이

--- baekjoon/string/ex24725.go
+++ baekjoon/string/ex24725.go
@@ -34,32 +34,34 @@ func main() {
 
 	//가로
 	if m > 3 {
-		result += readWordHorizontally(sl)
+		result += readMBTIWordHorizontally(sl)
 	}
 
 	//세로
 	if n > 3 {
-		result += readWordVertically(sl, m)
+		result += readMBTIWordVertically(sl, m)
 	}
 
+	//대각선
 	if n > 3 && m > 3 {
-		//대각선
-
+		result += readMBTIWordDiagonally()
 	}
+
+	fmt.Println(result)
 }
 
 func countMBTIs(word string) int {
 
 	var result int
 
-	for _, mbti := range mbtis {
-		result += strings.Count(word, mbti)
+	for _, m := range mbtis {
+		result += strings.Count(word, m)
 	}
 
 	return result
 }
 
-func readWordHorizontally(sl [][]string) int {
+func readMBTIWordHorizontally(sl [][]string) int {
 
 	var result int
 
@@ -70,7 +72,7 @@ func readWordHorizontally(sl [][]string) int {
 	return result
 }
 
-func readWordVertically(sl [][]string, m int) int {
+func readMBTIWordVertically(sl [][]string, m int) int {
 
 	var result int
 
@@ -85,5 +87,10 @@ func readWordVertically(sl [][]string, m int) int {
 		result += countMBTIs(word.String())
 	}
 
+	return result
+}
+
+func readMBTIWordDiagonally() int {
+	var result int
 	return result
 }
