5d2c0a7b91be6f5810cb8acbf8986cb50bd52ec5
thisisnew <thisisnew@naver.com>
2022-08-20 18:22:02 +0900

    개미굴

--- baekjoon/data/ex14725.go
+++ baekjoon/data/ex14725.go
@@ -31,17 +31,6 @@ func main() {
 	fmt.Println(result)
 }
 
-func generateBars(j int) string {
-
-	var result strings.Builder
-
-	for i := 0; i < 2*(j+1); i++ {
-		result.WriteString("-")
-	}
-
-	return result.String()
-}
-
 func getNewAntProperties(sl []string) []string {
 	var result = make([]string, len(sl)-2)
 
@@ -54,12 +43,25 @@ func getNewAntProperties(sl []string) []string {
 
 func addAntPropertiesToPrevAntProperties(sl, prev []string) []string {
 	for j := 0; j < len(sl)-2; j++ {
+		bars := generateBars(j)
+
 		if len(prev) >= j+1 {
-			prev[j] = fmt.Sprintf("%s%s%s", prev[j], generateBars(j), sl[j+2])
+			prev[j] = fmt.Sprintf("%s%s%s", prev[j], bars, sl[j+2])
 		} else {
-			prev = append(prev, fmt.Sprintf("%s%s", generateBars(j), sl[j+2]))
+			prev = append(prev, fmt.Sprintf("%s%s", bars, sl[j+2]))
 		}
 	}
 
 	return prev
+}
+
+func generateBars(j int) string {
+
+	var result strings.Builder
+
+	for i := 0; i < 2*(j+1); i++ {
+		result.WriteString("-")
+	}
+
+	return result.String()
 }
