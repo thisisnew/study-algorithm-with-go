33f530ca218bc101a782920b97ec25184224fbf9
thisisnew <thisisnew@naver.com>
2022-08-20 18:03:52 +0900

    개미굴

--- baekjoon/data/ex14725.go
+++ baekjoon/data/ex14725.go
@@ -13,10 +13,25 @@ func main() {
 	var n int
 	fmt.Fscanln(read, &n)
 
+	var result map[string][]string
+
 	for i := 0; i < n; i++ {
 		text, _, _ := read.ReadLine()
 		sl := strings.Split(string(text), " ")
-		m := sl[0]
+
+		pr, ok := result[sl[1]]
+
+		if !ok {
+			var props = make([]string, len(sl)-2)
+
+			for j := 0; j < len(sl)-2; j++ {
+				props[j] = sl[j+2]
+			}
+
+			result[sl[1]] = props
+		} else {
+
+		}
 
 	}
 
