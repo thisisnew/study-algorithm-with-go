f503c66589769e865f1249a72960bb0a42af4e96
thisisnew <thisisnew@naver.com>
2022-08-28 16:55:22 +0900

    탕수육

---
+++ baekjoon/string/ex13419.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	var read = bufio.NewReader(os.Stdin)
+
+	var t int
+	fmt.Fscanln(read, &t)
+
+	for i := 0; i < t; i++ {
+		var input string
+		fmt.Fscanln(read, &input)
+		fmt.Println(getFirstRepeatString(input))
+		fmt.Println(getSecondRepeatString(input))
+	}
+
+}
+
+func getFirstRepeatString(input string) string {
+
+	if len([]rune(input)) <= 1 {
+		return input
+	}
+
+	var result strings.Builder
+	var first = input[0:1]
+
+	var i = 0
+
+	for {
+		if i%2 != 0 {
+			continue
+		}
+
+		s := input[i : i+1]
+
+		if i > 0 && s == first {
+			break
+		}
+
+		result.WriteString(s)
+		i++
+	}
+
+	return result.String()
+}
+
+func getSecondRepeatString(input string) string {
+
+	if len([]rune(input)) <= 1 {
+		return input
+	}
+
+	var result strings.Builder
+	var second = input[1 : 1+1]
+
+	var i = 0
+
+	for {
+		if i%2 == 0 {
+			continue
+		}
+
+		s := input[i : i+1]
+
+		if s == second {
+			break
+		}
+
+		result.WriteString(s)
+		i++
+	}
+
+	return result.String()
+}
