ef5f4394796c82de0aae8e299533c349e9489c09
thisisnew <thisisnew@naver.com>
2022-08-29 06:23:41 +0900

    탕수육

--- baekjoon/string/ex13419.go
+++ baekjoon/string/ex13419.go
@@ -17,82 +17,22 @@ func main() {
 	for i := 0; i < t; i++ {
 		var input string
 		fmt.Fscanln(read, &input)
-		fmt.Println(getFirstRepeatString(input))
-		fmt.Println(getSecondRepeatString(input))
+
+		f, s := getRepeatString(getTsyStack(input))
+
+		fmt.Println(f)
+		fmt.Println(s)
 	}
 
 }
 
-func getFirstRepeatString(input string) string {
-
-	if len([]rune(input)) <= 1 {
-		return input
-	}
-
-	var result strings.Builder
-	var first = input[0:1]
-	var s = input[0:1]
-	var i = 0
-	var start = false
-
-	for {
-		if i%2 != 0 {
-			i++
-			continue
-		}
-
-		s = input[i : i+1]
-
-		if start && s == first {
-			break
-		}
-
-		i++
-
-		if i > len([]rune(input))-1 {
-			i = 0
-		}
-
-		result.WriteString(s)
-
-		if !start {
-			start = true
-		}
-	}
-
-	return result.String()
+func getTsyStack(input string) []rune {
+	return []rune(input)
 }
 
-func getSecondRepeatString(input string) string {
+func getRepeatString(tsyStack []rune) (string, string) {
 
-	if len([]rune(input)) <= 1 {
-		return input
-	}
+	var f strings.Builder
+	var s strings.Builder
 
-	var result strings.Builder
-	var second = input[1 : 1+1]
-	var i = 0
-
-	for {
-
-		if i > len([]rune(input))-1 {
-			i = 0
-		}
-
-		if i%2 == 0 {
-			i++
-			continue
-		}
-
-		s := input[i : i+1]
-
-		if s == second {
-			break
-		}
-
-		result.WriteString(s)
-		i++
-	}
-
-	return result.String()
 }
