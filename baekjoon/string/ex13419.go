00a7880ab02c10ebac5992b1367190926f55cab9
thisisnew <thisisnew@naver.com>
2022-08-28 17:04:04 +0900

    탕수육

--- baekjoon/string/ex13419.go
+++ baekjoon/string/ex13419.go
@@ -31,7 +31,7 @@ func getFirstRepeatString(input string) string {
 
 	var result strings.Builder
 	var first = input[0:1]
-
+	var s = input[0:1]
 	var i = 0
 
 	for {
@@ -40,18 +40,18 @@ func getFirstRepeatString(input string) string {
 			continue
 		}
 
-		s := input[i : i+1]
+		if i > len([]rune(input))-1 {
+			i = 0
+		}
 
-		if i > 0 && s == first {
+		i++
+		s = input[i : i+1]
+
+		if s == first {
 			break
 		}
 
 		result.WriteString(s)
-		i++
-
-		if i > len([]rune(input))-1 {
-			i = 0
-		}
 	}
 
 	return result.String()
