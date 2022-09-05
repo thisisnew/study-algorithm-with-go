8e75da762f2307657150cf915505e9c916e09789
thisisnew <thisisnew@naver.com>
2022-09-06 00:03:29 +0900

    학생 인기도 측정

--- baekjoon/string/ex25325.go
+++ baekjoon/string/ex25325.go
@@ -10,27 +10,22 @@ import (
 func main() {
 	var read = bufio.NewReader(os.Stdin)
 	var n int
-
 	fmt.Fscanln(read, &n)
 
-	var result map[string]int
+	text, _, _ := read.ReadLine()
+	var result = getPopularStudentMap(strings.Split(string(text), " "))
 
-	for i := 0; i <= n; i++ {
+	for i := 0; i < n; i++ {
 		text, _, _ := read.ReadLine()
 		students := strings.Split(string(text), " ")
 
-		if i == 0 {
-			result = getPopularStudentMap(students)
-		} else {
-
-			if len(students) <= 1 {
-				result[students[0]]++
-				continue
-			}
-
+		if len(students) <= 1 {
 			result[students[0]]++
-			result[students[1]]++
+			continue
 		}
+
+		result[students[0]]++
+		result[students[1]]++
 	}
 
 	fmt.Println(result)
