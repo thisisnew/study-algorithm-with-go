712a17d1facc861e6f05e66c18271b37b7317fc0
thisisnew <thisisnew@naver.com>
2022-09-19 00:00:36 +0900

    별 찍기 - 19

--- baekjoon/Implementation/ex10994.go
+++ baekjoon/Implementation/ex10994.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -18,7 +19,25 @@ func main() {
 
 	var max = 4*(n-1) + 1
 	for i := 0; i < max; i++ {
+		switch {
+		case i == 0, i == max-1:
+			printingStarsByNums(max)
+		case i%2 == 0:
+		default:
+
+		}
 
 	}
 
+}
+
+func printingStarsByNums(num int) {
+
+	var result strings.Builder
+
+	for i := 0; i < num; i++ {
+		result.WriteString("*")
+	}
+
+	fmt.Println(result.String())
 }
