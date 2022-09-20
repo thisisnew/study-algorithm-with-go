9c2055c93e91703912d613a0b1196c57cb96b63c
thisisnew <thisisnew@naver.com>
2022-09-20 13:31:01 +0900

    별 찍기 - 19

--- baekjoon/Implementation/ex10994.go
+++ baekjoon/Implementation/ex10994.go
@@ -18,6 +18,7 @@ func main() {
 	}
 
 	var max = 4*(n-1) + 1
+	var mid = max
 	for i := 0; i < max; i++ {
 		switch {
 		case i == 0, i == max-1:
@@ -25,10 +26,32 @@ func main() {
 		case i%2 == 0:
 			printingStarsByNumsEndToEnd(max)
 		default:
-
+			printingStarsByNumsByRules10994(max, mid)
 		}
 	}
 
+}
+
+func printingStarsByNumsByRules10994(max, mid int) {
+
+	var result strings.Builder
+
+	for i := 0; i < max; i++ {
+
+		if i == 0 || i == max-1 {
+			result.WriteString("*")
+			continue
+		}
+
+		if i == 1 || i == max-2 {
+			result.WriteString(" ")
+			continue
+		}
+
+		result.WriteString("*")
+	}
+
+	fmt.Println(result.String())
 }
 
 func printingStarsByNumsEndToEnd(num int) {
