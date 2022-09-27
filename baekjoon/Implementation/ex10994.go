c658b6b900a9fea05378696e0fd6b7f13de9a07d
thisisnew <thisisnew@naver.com>
2022-09-28 00:52:35 +0900

    별 찍기 - 19

--- baekjoon/Implementation/ex10994.go
+++ baekjoon/Implementation/ex10994.go
@@ -4,91 +4,47 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
 )
+
+var stars [][]string
 
 func main() {
 	var n int
 	var read = bufio.NewReader(os.Stdin)
 	fmt.Fscanln(read, &n)
 
-	if n == 1 {
-		fmt.Println("*")
+	var loops = 4*(n-1) + 1
+	stars = make([][]string, loops)
+
+	for i := 0; i < loops; i++ {
+		stars[i] = make([]string, loops)
+		for j := 0; j < loops; j++ {
+			stars[i][j] = " "
+		}
+	}
+
+	pointStars(0, loops)
+
+	for i := 0; i < loops; i++ {
+		for j := 0; j < loops; j++ {
+			fmt.Print(stars[i][j])
+		}
+		fmt.Println()
+	}
+}
+
+func pointStars(n, loops int) {
+
+	for i := n; i < loops; i++ {
+		stars[n][i] = "*"
+		stars[loops-1][i] = "*"
+		stars[i][n] = "*"
+		stars[i][loops-1] = "*"
+	}
+
+	if loops == 1 {
 		return
 	}
 
-	var loops = 4*(n-1) + 1
-	var stars = loops - 2
-
-	for i := 0; i < loops; i++ {
-
-		switch {
-		case i%2 == 0:
-			addStarsByNumsToBuilder(stars, loops)
-			if i < loops/2 {
-				stars -= 2
-			} else {
-				//반대방향
-			}
-		case i%2 != 0:
-			fmt.Println()
-			stars -= 2
-			//addStarsByNumsEndToEndToBuilder(stars, loops)
-		}
-
-	}
-
-}
-
-func addStarsByNumsEndToEndToBuilder(stars, loops int) {
-
-	var result strings.Builder
-
-	for i := 0; i < stars; i++ {
-
-		if i == 0 || i == stars-1 {
-			result.WriteString("*")
-			continue
-		}
-
-		result.WriteString(" ")
-	}
-
-	fmt.Println(result.String())
-}
-
-func addStarsByNumsToBuilder(stars, loops int) {
-
-	var result strings.Builder
-	dif := loops - stars
-	subDif := dif / 2
-	starCnt := 0
-	subDifCnt := 1
-	isBlank := false
-
-	for i := 0; i < loops; i++ {
-
-		if subDifCnt <= subDif {
-
-			if isBlank {
-				result.WriteString(" ")
-			} else {
-				result.WriteString("*")
-			}
-
-			isBlank = !isBlank
-			subDifCnt++
-		} else {
-			result.WriteString("*")
-			starCnt++
-		}
-
-		if subDifCnt >= subDif && starCnt == stars {
-			starCnt = 0
-			subDifCnt = 1
-			isBlank = !isBlank
-		}
-	}
-
-	fmt.Println(result.String())
+	pointStars(n+2, loops-2)
 }
