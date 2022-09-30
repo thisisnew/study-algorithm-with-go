2722fd8f7dce4a161264b6db35de0506a58dd0ab
thisisnew <thisisnew@naver.com>
2022-10-01 00:02:26 +0900

    별 찍기 - 17

---
+++ baekjoon/Implementation/ex10992.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	var n int
+	var read = bufio.NewReader(os.Stdin)
+	fmt.Fscanln(read, &n)
+
+	var loops = 2*(n-1) + 1
+	var mid = loops / 2
+	var left = mid
+	var right = mid
+
+	for i := 0; i < n; i++ {
+		var result strings.Builder
+
+		for j := 0; j < loops; j++ {
+
+			switch {
+			case i == 0:
+				if j == mid {
+					result.WriteString("*")
+					break
+				}
+
+				result.WriteString(" ")
+			case i == n-1:
+				result.WriteString("*")
+			default:
+				if j == left {
+					result.WriteString("*")
+					continue
+				}
+
+				if j == right {
+					result.WriteString("*")
+					break
+				}
+
+				result.WriteString(" ")
+			}
+		}
+
+		left--
+		right++
+		fmt.Println(result.String())
+		result.Reset()
+	}
+}
