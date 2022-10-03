eb4b8d5eaf9e4630104923466d227f038c27cda5
thisisnew <thisisnew@naver.com>
2022-10-03 11:10:41 +0900

    비밀번호 찾기

--- baekjoon/data/ex17219.go
+++ baekjoon/data/ex17219.go
@@ -6,25 +6,39 @@ import (
 	"os"
 )
 
+type urlPwInfo struct {
+	url string
+	pw  string
+}
+
 func main() {
 
 	var read = bufio.NewReader(os.Stdin)
 	var n, m int
 	fmt.Fscanln(read, &n, &m)
 
-	var urls = map[string]string{}
+	var urls = make([]urlPwInfo, n)
 	for i := 0; i < n; i++ {
 		var url string
 		var pw string
 		fmt.Fscanln(read, &url, &pw)
 
-		urls[url] = pw
+		urls[i] = urlPwInfo{url, pw}
 	}
 
 	for i := 0; i < m; i++ {
 		var url string
 		fmt.Fscanln(read, &url)
-		fmt.Println(urls[url])
+
+		for j := 0; j < len(urls); j++ {
+			if urls[j].url != url {
+				continue
+			}
+
+			fmt.Println(urls[j].pw)
+			break
+
+		}
 	}
 
 }
