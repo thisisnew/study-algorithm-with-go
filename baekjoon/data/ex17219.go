dd71a4fd052bf8ab8a66d4b3b80c328117c32c90
thisisnew <thisisnew@naver.com>
2022-10-03 09:20:14 +0900

    비밀번호 찾기

--- baekjoon/data/ex17219.go
+++ baekjoon/data/ex17219.go
@@ -12,12 +12,19 @@ func main() {
 	var n, m int
 	fmt.Fscanln(read, &n, &m)
 
-	loops := n + m
-	for i := 0; i < loops; i++ {
+	var urls = map[string]string{}
+	for i := 0; i < n; i++ {
 		var url string
 		var pw string
 		fmt.Fscanln(read, &url, &pw)
 
+		urls[url] = pw
+	}
+
+	for i := 0; i < m; i++ {
+		var url string
+		fmt.Fscanln(read, &url)
+		fmt.Println(urls[url])
 	}
 
 }
