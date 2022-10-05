2797d9408b749f98e9140d7fee8148124be57427
thisisnew <thisisnew@naver.com>
2022-10-06 00:05:37 +0900

    줄 세우기

--- baekjoon/data/ex2605.go
+++ baekjoon/data/ex2605.go
@@ -14,33 +14,11 @@ func main() {
 	var n int
 	fmt.Fscanln(read, &n)
 
-	var nums = make([]string, n)
-
 	input, _, _ := read.ReadLine()
 	inputs := strings.Split(string(input), " ")
 
-	for i, n := range inputs {
-		nums[i] = n
-	}
+	for i, num := range inputs {
 
-	sort.Slice(nums, func(i, j int) bool {
-		return nums[i] > nums[j]
-	})
-
-	for i, n := range nums {
-
-		for j, prop := range inputs {
-
-			if n == prop {
-				fmt.Print(j + 1)
-				break
-			}
-
-		}
-
-		if i < len(nums)-1 {
-			fmt.Print(" ")
-		}
 	}
 
 }
