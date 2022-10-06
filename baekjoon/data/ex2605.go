a7fd7c4a24a7826f8d47bd6a826415f619c15a6b
thisisnew <thisisnew@naver.com>
2022-10-06 17:01:52 +0900

    줄 세우기

--- baekjoon/data/ex2605.go
+++ baekjoon/data/ex2605.go
@@ -4,7 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"sort"
+	"strconv"
 	"strings"
 )
 
@@ -17,8 +17,29 @@ func main() {
 	input, _, _ := read.ReadLine()
 	inputs := strings.Split(string(input), " ")
 
+	var result []int
 	for i, num := range inputs {
+		n, _ = strconv.Atoi(num)
+		number := i + 1
 
+		switch {
+		case i == 0:
+			result = []int{number}
+		case n == 0:
+			result = append(result, number)
+		case n >= len(result):
+			result = append([]int{number}, result...)
+		default:
+			index := i - n
+			result = append(result[:index+1], result[index:]...)
+			result[index] = number
+		}
 	}
 
+	for i, n := range result {
+		fmt.Print(n)
+		if i < len(result)-1 {
+			fmt.Print(" ")
+		}
+	}
 }
