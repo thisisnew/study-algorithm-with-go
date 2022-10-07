2cb20422a319479570d70365f23fb6074b8e1063
thisisnew <thisisnew@naver.com>
2022-10-07 09:41:36 +0900

    걸그룹 마스터 준석이

---
+++ baekjoon/data/ex16165.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	var read = bufio.NewReader(os.Stdin)
+	var n, m int
+	fmt.Fscanln(read, &n, &m)
+
+	var groupNames = map[string][]string{}
+	var members = map[string]string{}
+
+	for i := 0; i < n; i++ {
+		var groupName string
+		fmt.Fscanln(read, &groupName)
+
+		var memberCount int
+		fmt.Fscanln(read, &memberCount)
+
+		var memberSl = make([]string, memberCount)
+		for j := 0; j < memberCount; j++ {
+			var member string
+			fmt.Fscanln(read, &member)
+			members[member] = groupName
+			memberSl[j] = member
+		}
+
+		groupNames[groupName] = memberSl
+	}
+
+	for i := 0; i < m; i++ {
+		var name string
+		fmt.Fscanln(read, &name)
+
+		var qType int
+		fmt.Fscanln(read, &qType)
+
+		switch qType {
+		case 0:
+			printGroupMembers(groupNames[name])
+		case 1:
+			printGroupName(members[name])
+		}
+	}
+
+}
+
+func printGroupMembers(members []string) {
+	for _, member := range members {
+		fmt.Println(member)
+	}
+}
+
+func printGroupName(groupName string) {
+	fmt.Println(groupName)
+}
