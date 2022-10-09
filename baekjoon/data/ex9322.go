5ccb870fa781897f6dec4c015d1e990907e539b0
thisisnew <thisisnew@naver.com>
2022-10-10 01:03:04 +0900

    철벽 보안 알고리즘

---
+++ baekjoon/data/ex9322.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	var read = bufio.NewReader(os.Stdin)
+
+	var t int
+	fmt.Fscanln(read, &t)
+
+	for i := 0; i < t; i++ {
+		var n int
+		fmt.Fscanln(read, &n)
+
+		var fPublicKey []string
+		var sPublicKey []string
+		var crypto []string
+
+		for j := 0; j < 3; j++ {
+			text, _, _ := read.ReadLine()
+			values := strings.Split(string(text), " ")
+
+			switch i {
+			case 0:
+				fPublicKey = values
+			case 1:
+				sPublicKey = values
+			case 2:
+				crypto = values
+			}
+		}
+
+		cccc(fPublicKey, sPublicKey, crypto)
+	}
+}
+
+func cccc(fPublicKey, sPublicKey, crypto []string) {
+
+}
