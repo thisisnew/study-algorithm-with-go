2b388dcf55b708e9a05e3b57c25a781c78a15726
thisisnew <thisisnew@naver.com>
2022-10-17 14:32:20 +0900

    삼총사

--- programmers/level1/삼총사.go
+++ programmers/level1/삼총사.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func main() {
 	nums := []int{
-		1, 2, 3, 4,
+		-2, 3, 0, 2, -5,
 	}
 
 	fmt.Println(삼총사(nums))
@@ -15,20 +15,19 @@ func 삼총사(nums []int) int {
 	var result int
 
 	for i := 0; i < len(nums); i++ {
-		var r = i
 	se:
 		for j := 0; j < len(nums); j++ {
 			if i == j {
 				continue se
 			}
-			r += j
 		th:
 			for k := 0; k < len(nums); k++ {
 				if k == i || k == j {
 					continue th
 				}
 
-				if r+k == 0 {
+				r := nums[i] + nums[j] + nums[k]
+				if r == 0 {
 					result++
 				}
 			}
