3d7599b10ab23b7c8eeffe0322ccefa3fbaae29b
thisisnew <thisisnew@naver.com>
2022-10-19 09:38:04 +0900

    성격 유형 검사하기

---
+++ programmers/level1/성격유형검사하기.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	survey := []string{
+		"",
+	}
+
+	choices := []int{
+		-1, 1, -1, 1,
+	}
+
+	fmt.Println(성격유형검사하기(survey, choices))
+}
+
+func 성격유형검사하기(survey []string, choices []int) string {
+
+	var result string
+
+	return result
+}
