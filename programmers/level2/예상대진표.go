a1c57909246de7b9dd878675b78321a94bdad286
thisisnew <thisisnew@naver.com>
2022-10-20 21:56:28 +0900

    예상 대진표

--- programmers/level2/예상대진표.go
+++ programmers/level2/예상대진표.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
@@ -11,13 +10,30 @@ func main() {
 
 func 예상대진표(n int, a int, b int) int {
 
-	var result int
+	var round = 1
 
 	for n > 1 {
-		result++
 
-		if math.Abs(float64(a-b)) == 1 {
-			return result
+		var teams = make([][]int, n/2)
+		var team = make([]int, 2)
+		var idx = 0
+
+		for i := 0; i < n; i++ {
+			team[i%2] = i + 1
+
+			if i%2 > 0 {
+				teams[idx] = team
+				idx++
+				team = make([]int, 2)
+			}
+		}
+
+		for _, team := range teams {
+			if !isMatching(team, a, b) {
+				continue
+			}
+
+			return round
 		}
 
 		if a%2 == 0 {
@@ -33,7 +49,27 @@ func 예상대진표(n int, a int, b int) int {
 		}
 
 		n = n / 2
+
+		round++
 	}
 
-	return result
+	return round
+}
+
+func isMatching(team []int, a, b int) bool {
+
+	isA := false
+	isB := false
+
+	for _, t := range team {
+		if t == a {
+			isA = true
+		}
+
+		if t == b {
+			isB = true
+		}
+	}
+
+	return isA && isB
 }
