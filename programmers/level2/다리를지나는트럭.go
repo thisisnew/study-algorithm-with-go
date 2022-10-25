e9bf25485f57dfc5584847103cd20e0f2ab43e83
thisisnew <thisisnew@naver.com>
2022-10-26 00:12:37 +0900

    다리를 지나는 트럭

--- programmers/level2/다리를지나는트럭.go
+++ programmers/level2/다리를지나는트럭.go
@@ -4,9 +4,9 @@ import "fmt"
 
 func main() {
 
-	bridgeLength := 2
-	weight := 10
-	truckWeights := []int{7, 4, 5, 6}
+	bridgeLength := 2                 // 다리에 올라갈 수 있는 트럭 수
+	weight := 10                      // 다리가 견딜 수 있는 무게
+	truckWeights := []int{7, 4, 5, 6} //트럭 별 무게
 
 	fmt.Println(다리를지나는트럭(bridgeLength, weight, truckWeights))
 
@@ -15,35 +15,64 @@ func main() {
 func 다리를지나는트럭(bridgeLength int, weight int, truckWeights []int) int {
 
 	var result int
-	var cTrucks []int
+	var truckCount = len(truckWeights)
+	var passingTrucks []int
+	var passedTrucks []int
 
-	result++
+	for {
+		if len(passingTrucks) < bridgeLength {
+			//하나더 진행
+			result++
+			passingTrucks = append(passingTrucks, truckWeights[0])
+			truckWeights = passTruck(truckWeights)
 
-	totalWeight := getTotalTrucksWeight(cTrucks)
+			if sumTruckWeight(passingTrucks) <= weight {
+				continue
+			} else {
+				//되돌려놓기
+				truckWeights = append([]int{passingTrucks[len(passingTrucks)-1]}, truckWeights...)
+				passingTrucks = passingTrucks[:len(passingTrucks)-1]
+				result++
 
-	if totalWeight < weight {
+				//각각 한칸앞으로
+				passedTrucks = append(passedTrucks, passingTrucks[0])
+				passingTrucks = passTruck(passingTrucks)
+				passingTrucks = append(passingTrucks, truckWeights[0])
+				truckWeights = passTruck(truckWeights)
+				result++
+			}
 
+		} else {
+			//한칸앞으로
+			passedTrucks = append(passedTrucks, passingTrucks[0])
+			passingTrucks = passTruck(passingTrucks)
+			result++
+		}
+
+		if len(passedTrucks) == truckCount {
+			return result
+		}
 	}
 
-	return result
 }
 
-func getTotalTrucksWeight(cTrucks []int) int {
+func passTruck(truck []int) []int {
+
+	if len(truck) == 0 {
+		return []int{}
+	}
+
+	return truck[1:]
+}
+
+func sumTruckWeight(passingTrucks []int) int {
 
 	var result int
 
-	for _, c := range cTrucks {
-		result += c
+	for _, p := range passingTrucks {
+		result += p
 	}
 
 	return result
-
-}
-
-func crossingTheBridge() {
-
-}
-
-func waitingTrucks() {
 
 }
