9ba7aa875b61374e205f8bdcb8a305352746d11e
thisisnew <thisisnew@naver.com>
2022-10-31 23:52:39 +0900

    옹알이2

--- programmers/level1/옹알이2.go
+++ programmers/level1/옹알이2.go
@@ -26,38 +26,11 @@ func canBabbling(bs string) bool {
 	var possible = []string{"aya", "ye", "woo", "ma"}
 
 	for _, p := range possible {
-		var temp strings.Builder
 
-	lp:
-		for i := 0; i < len([]rune(bs))-1; i++ {
+		if strings.Contains(bs, p) {
 
-			if bs[i:i+1] != p[0:1] {
-				continue
-			}
-
-			temp.WriteString(bs[i : i+1])
-
-			for j := i + 1; j < len([]rune(bs)); j++ {
-				temp.WriteString(bs[j : j+1])
-
-				if len([]rune(temp.String())) == len([]rune(p)) {
-
-					t := temp.String()
-					temp.Reset()
-
-					if t == p {
-						bs = bs[j+1:]
-
-						if bs == "" {
-							return true
-						}
-
-						continue lp
-
-					}
-				}
-			}
 		}
+
 	}
 
 	return false
