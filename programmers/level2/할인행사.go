439b1669fb49ee137508118bd38f4777df2c0719
thisisnew <thisisnew@naver.com>
2022-11-02 00:19:25 +0900

    할인 행사

--- programmers/level2/할인행사.go
+++ programmers/level2/할인행사.go
@@ -13,5 +13,50 @@ func main() {
 }
 
 func 할인행사(want []string, number []int, discount []string) int {
-	return 0
+
+	var result int
+	wantMap := getWantMap(want, number)
+
+	lp := len(discount) - 10
+	for i := 0; i < lp; i++ {
+		tenDayDiscount := discount[i : i+10]
+		dWantMap := decreaseWandFromDiscount(wantMap, tenDayDiscount)
+
+		if canAllBuy(dWantMap) {
+			result++
+		}
+
+	}
+
+	return result
+}
+func getWantMap(want []string, number []int) map[string]int {
+
+	var result = make(map[string]int)
+
+	for i, w := range want {
+		result[w] = number[i]
+	}
+
+	return result
+}
+
+func decreaseWandFromDiscount(wantMap map[string]int, tenDayDiscount []string) map[string]int {
+
+	for _, d := range tenDayDiscount {
+		wantMap[d]--
+	}
+
+	return wantMap
+}
+
+func canAllBuy(wantMap map[string]int) bool {
+
+	for _, v := range wantMap {
+		if v > 0 {
+			return false
+		}
+	}
+
+	return true
 }
