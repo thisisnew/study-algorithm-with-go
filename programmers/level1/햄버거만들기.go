2d46b037125644095f918ca74dde58d9dc1eedb6
thisisnew <thisisnew@naver.com>
2022-11-07 00:11:20 +0900

    햄버거 만들기

--- programmers/level1/햄버거만들기.go
+++ programmers/level1/햄버거만들기.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
-const bread = 1
-const vegetable = 2
-const meat = 3
+const bread = "1"
+const vegetable = "2"
+const meat = "3"
 
 func main() {
 	fmt.Println(햄버거만들기([]int{2, 1, 1, 2, 3, 1, 2, 3, 1}))
@@ -16,9 +17,10 @@ func 햄버거만들기(ingredient []int) int {
 
 	var result = 0
 	var isBurgerMade = true
+	var ingredientStr = getIngredientString(ingredient)
 
 	for {
-		ln := len(ingredient)
+		ln := len([]rune(ingredientStr))
 
 		if ln < 4 || !isBurgerMade {
 			break
@@ -26,25 +28,25 @@ func 햄버거만들기(ingredient []int) int {
 
 		isBurgerMade = false
 
-		for i := 0; i < len(ingredient); i++ {
-			if ingredient[i] != bread {
+		for i := 0; i < ln; i++ {
+			if ingredientStr[i:i+1] != bread {
 				continue
 			}
 
 			if isAnyIngredientRemain(ln, i) {
-				if ingredient[i+1] != vegetable {
+				if ingredientStr[i+1:i+2] != vegetable {
 					continue
 				}
 
-				if ingredient[i+2] != meat {
+				if ingredientStr[i+2:i+3] != meat {
 					continue
 				}
 
-				if ingredient[i+3] != bread {
+				if ingredientStr[i+3:i+4] != bread {
 					continue
 				}
 
-				ingredient = append(ingredient[:i], ingredient[i+4:]...)
+				ingredientStr = ingredientStr[:i] + ingredientStr[i+4:]
 				isBurgerMade = true
 				result++
 				break
@@ -53,6 +55,22 @@ func 햄버거만들기(ingredient []int) int {
 	}
 
 	return result
+}
+
+func getIngredientString(ingredient []int) string {
+
+	var sum int
+	var last = len(ingredient) - 1
+	for i, p := range ingredient {
+		sum += p
+
+		if i < last {
+			sum *= 10
+		}
+
+	}
+
+	return strconv.Itoa(sum)
 }
 
 func isAnyIngredientRemain(ln, idx int) bool {
