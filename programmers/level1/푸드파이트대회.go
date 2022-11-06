b1b7e9fe4b8ae850b8f1a6a87efe8ad227d5e0ba
thisisnew <thisisnew@naver.com>
2022-11-06 15:22:05 +0900

    푸드 파이트 대회

--- programmers/level1/푸드파이트대회.go
+++ programmers/level1/푸드파이트대회.go
@@ -1,11 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 func main() {
 	fmt.Println(푸드파이트대회([]int{1, 3, 4, 6}))
 }
 
 func 푸드파이트대회(food []int) string {
-	return ""
+
+	var result strings.Builder
+
+	displayOne := displayDishes(food)
+
+	result.WriteString(displayOne)
+	result.WriteString("0")
+	result.WriteString(displayDishesAnother(displayOne))
+
+	return result.String()
+}
+
+func displayDishes(food []int) string {
+
+	var result strings.Builder
+
+	for i := 1; i < len(food); i++ {
+		cnt := food[i] / 2
+		food := strconv.Itoa(i)
+
+		for j := 0; j < cnt; j++ {
+			result.WriteString(food)
+		}
+	}
+
+	return result.String()
+}
+
+func displayDishesAnother(displayOne string) string {
+
+	var result strings.Builder
+
+	for i := len(displayOne) - 1; i >= 0; i-- {
+		result.WriteString(displayOne[i : i+1])
+	}
+
+	return result.String()
 }
