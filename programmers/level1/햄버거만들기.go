6288294a2d5e064d5110f10e0e58a9bbe460cb25
thisisnew <thisisnew@naver.com>
2022-11-07 15:04:49 +0900

    햄버거 만들기

--- programmers/level1/햄버거만들기.go
+++ programmers/level1/햄버거만들기.go
@@ -6,27 +6,71 @@ import (
 	"strings"
 )
 
+const bread = "1"
+const vegetable = "2"
+const meat = "3"
+
 func main() {
 	fmt.Println(햄버거만들기([]int{2, 1, 1, 2, 3, 1, 2, 3, 1}))
 }
 
 func 햄버거만들기(ingredient []int) int {
 
-	var result int
-	var cIngredient = ingredientToString(ingredient)
+	var result = 0
+	var isBurgerMade = true
+	var ingredientStr = getIngredientString(ingredient)
 
 	for {
-		nIngredient := strings.Replace(cIngredient, "1231", "", 1)
+		ln := len([]rune(ingredientStr))
 
-		if nIngredient == cIngredient {
-			return result
+		if ln < 4 || !isBurgerMade {
+			break
 		}
-		cIngredient = nIngredient
-		result++
+
+		isBurgerMade = false
+		var i = 0
+		for {
+
+			if i >= ln {
+				break
+			}
+
+			if !isAnyIngredientRemain(ln, i) {
+				break
+			}
+
+			if ingredientStr[i:i+1] != bread {
+				i++
+				continue
+			}
+
+			if ingredientStr[i+1:i+2] != vegetable {
+				i++
+				continue
+			}
+
+			if ingredientStr[i+2:i+3] != meat {
+				i += 2
+				continue
+			}
+
+			if ingredientStr[i+3:i+4] != bread {
+				i += 3
+				continue
+			}
+
+			ingredientStr = ingredientStr[:i] + ingredientStr[i+4:]
+			isBurgerMade = true
+			result++
+			break
+
+		}
 	}
+
+	return result
 }
 
-func ingredientToString(ingredient []int) string {
+func getIngredientString(ingredient []int) string {
 
 	var result strings.Builder
 
@@ -35,5 +79,8 @@ func ingredientToString(ingredient []int) string {
 	}
 
 	return result.String()
+}
 
+func isAnyIngredientRemain(ln, idx int) bool {
+	return idx+3 <= ln-1
 }
