25c8345049d93ffed1eace9b6442e4ee725f14be
thisisnew <thisisnew@naver.com>
2022-11-11 00:07:00 +0900

    혼자 놀기의 달인

--- programmers/level2/혼자놀기의달인.go
+++ programmers/level2/혼자놀기의달인.go
@@ -6,70 +6,53 @@ import (
 )
 
 func main() {
-	fmt.Println(혼자놀기의달인([]int{8, 6, 3, 7, 2, 5, 1, 4}))
+	fmt.Println(혼자놀기의달인([]int{2, 3, 1}))
 }
 
 func 혼자놀기의달인(cards []int) int {
 
-	var results []int
+	var openedBoxMap = map[int]bool{}
+	var boxPoints []int
 
-	for {
+	for i := 0; i < len(cards); i++ {
+		var boxPoint = 0
 
-		var openedBoxMap = map[int]bool{}
-		var boxPoints []int
+		isOpened, ok := openedBoxMap[i]
 
-		for i := 0; i < len(cards); i++ {
-			var boxPoint = 0
+		if ok && isOpened {
+			continue
+		}
 
-			isOpened, ok := openedBoxMap[i]
+		boxNum := cards[i]
+		openedBoxMap[boxNum] = true
+		boxPoint++
+
+		for {
+			boxNum = cards[boxNum-1]
+			isOpened, ok = openedBoxMap[boxNum]
 
 			if ok && isOpened {
-				continue
+				boxPoints = append(boxPoints, boxPoint)
+				break
 			}
 
-			boxNum := cards[i]
 			openedBoxMap[boxNum] = true
 			boxPoint++
-
-			for {
-				boxNum = cards[boxNum-1]
-				isOpened, ok = openedBoxMap[boxNum]
-
-				if ok && isOpened {
-					boxPoints = append(boxPoints, boxPoint)
-					break
-				}
-
-				openedBoxMap[boxNum] = true
-				boxPoint++
-			}
 		}
-
-		results = append(results, calculateHighestBoxPoint(boxPoints))
-
-		if len(results) == len(cards) {
-			return getHighestBoxPoint(results)
-		}
-
-		c := cards[0]
-		cards = cards[1:]
-		cards = append(cards, c)
 
 	}
+
+	return getHighestBoxPoint(boxPoints)
 }
 
-func getHighestBoxPoint(results []int) int {
+func getHighestBoxPoint(boxPoints []int) int {
 
-	sort.Slice(results, func(i, j int) bool {
-		return results[i] > results[j]
-	})
-
-	return results[0]
-}
-
-func calculateHighestBoxPoint(boxPoints []int) int {
+	if len(boxPoints) == 0 {
+		return 0
+	}
 
 	if len(boxPoints) <= 1 {
+		return boxPoints[0]
 		return 0
 	}
 
