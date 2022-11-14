d9a7a04080c20a1d37aa4e896c84ca4c224d30c2
thisisnew <thisisnew@naver.com>
2022-11-15 01:27:12 +0900

    교점에 별 만들기

--- programmers/level2/양궁대회.go
+++ programmers/level2/양궁대회.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println(양궁대회(5, []int{2, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0}))
@@ -14,7 +16,21 @@ func 양궁대회(n int, info []int) []int {
 		return winOverZeroPoint(n, info)
 	}
 
-	return []int{}
+	var result = make([]int, len(info))
+
+	for i, p := range info {
+
+		v := p + 1
+		n -= v
+
+		if n < 0 {
+			break
+		}
+
+		result[i] = v
+	}
+
+	return result
 }
 
 func countZeroPoint(info []int) int {
@@ -47,6 +63,21 @@ func winOverZeroPoint(n int, info []int) []int {
 
 	if remain > 0 {
 		result[len(result)-1] += remain
+	}
+
+	return result
+}
+
+func getNonZeroPointInfo(info []int) []int {
+
+	var result []int
+
+	for _, p := range info {
+		if p == 0 {
+			continue
+		}
+
+		result = append(result, p)
 	}
 
 	return result
