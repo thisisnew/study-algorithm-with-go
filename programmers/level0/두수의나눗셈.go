671ffe201815709fc9ffa21d3f0a4f660639b62a
thisisnew <thisisnew@naver.com>
2022-11-17 19:27:29 +0900

    두 수의 나눗셈

--- programmers/level0/두수의나눗셈.go
+++ programmers/level0/두수의나눗셈.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(3, 2)
+	fmt.Println(두수의나눗셈(3, 2))
 }
 
 func 두수의나눗셈(num1 int, num2 int) int {
-	return (num2 / num1) * 1000
+	return num1 * 1000 / num2
 }
