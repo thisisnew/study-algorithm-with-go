ea97277b8a1967db2885421ef2e17868b5deed54
thisisnew <thisisnew@naver.com>
2022-11-17 12:03:35 +0900

    두 큐 합 같게 만들기

--- programmers/level2/두큐합같게만들기.go
+++ programmers/level2/두큐합같게만들기.go
@@ -58,6 +58,7 @@ func 두큐합같게만들기(queue1 []int, queue2 []int) int {
 	q2 := Queue{queue2}
 
 	middleValue, remain := getMiddleValue(q1, q2)
+	limitCount := len(q1.items) + len(q2.items)
 
 	if remain == 1 {
 		return -1
@@ -66,6 +67,11 @@ func 두큐합같게만들기(queue1 []int, queue2 []int) int {
 	var result int
 
 	for {
+
+		if limitCount < 0 {
+			return -1
+		}
+
 		switch {
 		case q2.sum() > q1.sum():
 			p := q2.pop()
@@ -82,6 +88,7 @@ func 두큐합같게만들기(queue1 []int, queue2 []int) int {
 		}
 
 		result++
+		limitCount--
 	}
 }
 
