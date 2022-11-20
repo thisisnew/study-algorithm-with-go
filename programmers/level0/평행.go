be7a2e2ff383656ffc3dcfb48465f737c284de47
thisisnew <thisisnew@naver.com>
2022-11-21 01:17:20 +0900

    평행

--- programmers/level0/평행.go
+++ programmers/level0/평행.go
@@ -21,15 +21,21 @@ func 평행(dots [][]int) int {
 	dot4 := dots[3]
 
 	if getInclinationDots(dot1, dot2, dot3, dot4) {
-
+		if (dot4[1]-dot3[1])/(dot2[1]-dot1[1]) != -1 {
+			return 1
+		}
 	}
 
 	if getInclinationDots(dot1, dot3, dot2, dot4) {
-
+		if (dot4[1]-dot2[1])/(dot3[1]-dot1[1]) != -1 {
+			return 1
+		}
 	}
 
-	if getInclinationDots(dot1, dot4, dot3, dot4) {
-
+	if getInclinationDots(dot1, dot4, dot2, dot3) {
+		if (dot3[1]-dot2[1])/(dot4[1]-dot1[1]) != -1 {
+			return 1
+		}
 	}
 
 	return 0
