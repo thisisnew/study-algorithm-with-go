55fd0a98572555ea233bd8b3219afc1f4429a49b
thisisnew <thisisnew@naver.com>
2022-11-24 00:08:56 +0900

    겹치는 선분의 길이

--- programmers/level0/겹치는선분의길이.go
+++ programmers/level0/겹치는선분의길이.go
@@ -61,5 +61,5 @@ func 겹치는선분의길이(lines [][]int) int {
 }
 
 func isContainLine(line1, line2, line3 []int) bool {
-	return line1[0] <= line2[0] && line1[0] <= line3[0] && line1[1] >= line2[1] && line1[1] >= line3[1]
+	return (line1[0] <= line2[0] && line1[1] >= line2[1]) || (line1[0] <= line3[0] && line1[1] >= line3[1])
 }
