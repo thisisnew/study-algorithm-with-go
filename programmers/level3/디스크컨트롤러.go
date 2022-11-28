5bed0ed2daa5c63156e3d659e41880704edc3a9a
thisisnew <thisisnew@naver.com>
2022-11-28 10:54:43 +0900

    디스크 컨트롤러

--- programmers/level3/디스크컨트롤러.go
+++ programmers/level3/디스크컨트롤러.go
@@ -1,11 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 func main() {
 	fmt.Println(디스크컨트롤러([][]int{{0, 3}, {1, 9}, {2, 6}}))
 }
 
 func 디스크컨트롤러(jobs [][]int) int {
+
+	var jobTimes = make([]int, len(jobs))
+
+	for i, job := range jobs {
+		jobTimes[i] = job[1] - job[0]
+	}
+
+	sort.Slice(jobTimes, func(i, j int) bool {
+		return jobTimes[i] < jobTimes[j]
+	})
+
 	return 0
 }
