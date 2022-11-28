250cd969335004dc619b9fb66d75fcf1a5afe4d3
thisisnew <thisisnew@naver.com>
2022-11-28 18:09:46 +0900

    디스크 컨트롤러

--- programmers/level3/디스크컨트롤러.go
+++ programmers/level3/디스크컨트롤러.go
@@ -11,15 +11,45 @@ func main() {
 
 func 디스크컨트롤러(jobs [][]int) int {
 
-	var jobTimes = make([]int, len(jobs))
+	jobTimes := getJobTimesAsc(jobs)
+	var jobsAsc [][]int
 
-	for i, job := range jobs {
-		jobTimes[i] = job[1] - job[0]
+	for _, jobTime := range jobTimes {
+		jobsAsc = append(jobsAsc, getJobsAsc(jobTime, jobs)...)
 	}
 
-	sort.Slice(jobTimes, func(i, j int) bool {
-		return jobTimes[i] < jobTimes[j]
+	return 0
+}
+
+func getJobTimesAsc(jobs [][]int) []int {
+	var result = make([]int, len(jobs))
+
+	for i, job := range jobs {
+		result[i] = job[1] - job[0]
+	}
+
+	sort.Slice(result, func(i, j int) bool {
+		return result[i] < result[j]
 	})
 
-	return 0
+	return result
+}
+
+func getJobsAsc(jobTime int, jobs [][]int) [][]int {
+
+	var result [][]int
+
+	for _, job := range jobs {
+		if job[1]-job[0] != jobTime {
+			continue
+		}
+
+		result = append(result, job)
+	}
+
+	sort.Slice(result, func(i, j int) bool {
+		return result[i][0] < result[j][0]
+	})
+
+	return result
 }
