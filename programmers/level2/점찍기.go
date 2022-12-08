e06947eb55fc4b4473bb1dfc4602d704f8446490
thisisnew <thisisnew@naver.com>
2022-12-09 00:10:44 +0900

    점 찍기

---
+++ programmers/level2/점찍기.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(점찍기(1, 5))
+}
+
+func 점찍기(k int, d int) int64 {
+
+	var result int64
+	var x int
+	var y int
+
+	for {
+		if x*x > d*d && y*y > d*d {
+			return result
+		}
+
+		if (x*x)+(y*y) > d*d {
+			x += k
+			y = 0
+		}
+
+		if (x*x)+(y*y) <= d*d {
+			result++
+			y += k
+		}
+	}
+}
