3bcfdf8d1a93f3dcd1f0dad7b0d9d7eaf439defd
thisisnew <thisisnew@naver.com>
2022-12-14 00:00:14 +0900

    dfs 연습

--- baekjoon/dfs/ex1260.go
+++ baekjoon/dfs/ex1260.go
@@ -7,29 +7,26 @@ import (
 )
 
 var (
-	graph   [][]bool
-	visited []bool
-	writer  *bufio.Writer
+	graph     [][]bool
+	isVisited []bool
 )
 
 func main() {
-
 	var n, m, v int
 	var read = bufio.NewReader(os.Stdin)
-	writer = bufio.NewWriter(os.Stdout)
-	defer writer.Flush()
-
 	fmt.Fscanln(read, &n, &m, &v)
-	graph = make([][]bool, n+1)
-	visited = make([]bool, n+1)
 
-	for i, _ := range graph {
+	graph = make([][]bool, n+1)
+	for i := range graph {
 		graph[i] = make([]bool, n+1)
 	}
+
+	isVisited = make([]bool, n+1)
 
 	for i := 0; i < m; i++ {
 		var p1, p2 int
 		fmt.Fscanln(read, &p1, &p2)
+
 		graph[p1][p2] = true
 		graph[p2][p1] = true
 	}
@@ -38,12 +35,13 @@ func main() {
 }
 
 func dfs(v int) {
-	visited[v] = true // dfs에 들어오면 방문했다고 판단
-	fmt.Fprintf(writer, "%d ", v)
+	isVisited[v] = true
+
+	fmt.Print(v, " ")
 
 	for i := 0; i < len(graph[v]); i++ {
-		if graph[v][i] && !visited[i] { // 연결되어 있고 방문하지 않은 경우
-			dfs(i) // 재귀함수 호출
+		if graph[v][i] && !isVisited[i] {
+			dfs(i)
 		}
 	}
 }
