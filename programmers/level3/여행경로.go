183167440dc09ff96a5381171545acf97d5d1159
thisisnew <thisisnew@naver.com>
2022-12-16 00:15:13 +0900

    여행 경로

---
+++ programmers/level3/여행경로.go
@@ -0,0 +1,29 @@
+package main
+
+func main() {
+
+}
+
+var (
+	isVisitedTicket []bool
+	graphTickets    [][]string
+)
+
+func 여행경로(tickets [][]string) []string {
+	isVisitedTicket = make([]bool, len(tickets)+1)
+	graphTickets = make([][]string, len(tickets)+1)
+
+	for i := range graphTickets {
+		graphTickets[i] = make([]string, len(tickets)+1)
+	}
+
+	for _, ticket := range tickets {
+		graphTickets[]
+	}
+
+	return []string{}
+}
+
+func dfsTickets(v int) {
+	isVisitedTicket[v] = true
+}
