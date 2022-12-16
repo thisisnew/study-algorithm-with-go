616e42c1872acc139cfa93ddd2a6a79b0ece0e17
thisisnew <thisisnew@naver.com>
2022-12-16 11:12:26 +0900

    DFS와 BFS

--- baekjoon/dfs/ex1260.go
+++ baekjoon/dfs/ex1260.go
@@ -6,44 +6,43 @@ import (
 	"os"
 )
 
-var (
+type Graph1260 struct {
 	graph     [][]bool
 	isVisited []bool
-	writer    *bufio.Writer
-)
+}
 
 func main() {
 	var n, m, v int
 	var read = bufio.NewReader(os.Stdin)
 	fmt.Fscanln(read, &n, &m, &v)
 
-	writer = bufio.NewWriter(os.Stdout)
-
-	graph = make([][]bool, n+1)
-	for i := range graph {
-		graph[i] = make([]bool, n+1)
+	var graph1260 = Graph1260{
+		graph:     make([][]bool, n+1),
+		isVisited: make([]bool, n+1),
 	}
 
-	isVisited = make([]bool, n+1)
+	for i := range graph1260.graph {
+		graph1260.graph[i] = make([]bool, n+1)
+	}
 
 	for i := 0; i < m; i++ {
 		var p1, p2 int
 		fmt.Fscanln(read, &p1, &p2)
 
-		graph[p1][p2] = true
-		graph[p2][p1] = true
+		graph1260.graph[p1][p2] = true
+		graph1260.graph[p2][p1] = true
 	}
 
-	dfs(v)
+	dfs(v, &graph1260)
 }
 
-func dfs(v int) {
-	isVisited[v] = true
-	fmt.Fprintf(writer, "%d", v)
+func dfs(v int, graph1260 *Graph1260) {
+	graph1260.isVisited[v] = true
+	fmt.Print(v, " ")
 
-	for i := 0; i < len(graph[v]); i++ {
-		if graph[v][i] && !isVisited[i] {
-			dfs(i)
+	for i := 0; i < len(graph1260.graph[v]); i++ {
+		if graph1260.graph[v][i] && !graph1260.isVisited[i] {
+			dfs(i, graph1260)
 		}
 	}
 }
