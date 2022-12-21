b4bc57f84b925b041c22c8490c576e86cb4ba981
thisisnew <thisisnew@naver.com>
2022-12-22 00:02:09 +0900

    단어 변환

---
+++ programmers/level3/단어변환.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(단어변환("hit", "cog", []string{"hot", "dot", "dog", "lot", "log", "cog"}))
+}
+
+var (
+	wordDfsResult int
+)
+
+func 단어변환(begin string, target string, words []string) int {
+
+	return wordDfsResult
+}
+
+func wordDfs() {
+	wordDfsResult++
+
+}
