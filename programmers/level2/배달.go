6390e2e530412d4b9b96ad52c750859e323698b2
thisisnew <thisisnew@naver.com>
2022-12-23 20:58:34 +0900

    배달

---
+++ programmers/level2/배달.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(배달(5, [][]int{{1, 2, 1}, {2, 3, 3}, {5, 2, 2}, {1, 4, 2}, {5, 3, 1}, {5, 4, 2}}, 3))
+}
+
+func 배달(N int, road [][]int, k int) int {
+	answer := 0
+
+	// [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+	fmt.Printf("Hello Go")
+
+	return answer
+}
