0b5ea83158c5c5fcfcd2a636b8bfe112f7e55f95
thisisnew <thisisnew@naver.com>
2022-12-23 22:06:29 +0900

    DNA

--- baekjoon/string/ex1969.go
+++ baekjoon/string/ex1969.go
@@ -20,49 +20,49 @@ func main() {
 		dnas[i] = dna
 	}
 
-	var hd int
+	var hammingDistance int
 	var result strings.Builder
-	var idx int
 
-	for idx < n {
+	for _, dna := range dnas {
+
 		var a, c, g, t int
 		var max = 0
 		var maxC string
 
-		for _, dna := range dnas {
-			switch dna[idx : idx+1] {
-			case "A":
+		for _, token := range dna {
+
+			switch token {
+			case 'A':
 				a++
 				if max < a {
 					max = a
 					maxC = "A"
 				}
-			case "C":
+			case 'C':
 				c++
 				if max < c {
 					max = c
 					maxC = "C"
 				}
-			case "G":
+			case 'G':
 				g++
 				if max < g {
 					max = g
 					maxC = "G"
 				}
-			case "T":
+			case 'T':
 				t++
 				if max < t {
 					max = t
 					maxC = "T"
 				}
 			}
-			idx++
 		}
 
 		result.WriteString(maxC)
-		hd = n - max
+		hammingDistance = n - max
 	}
 
 	fmt.Println(result.String())
-	fmt.Println(hd)
+	fmt.Println(hammingDistance)
 }
