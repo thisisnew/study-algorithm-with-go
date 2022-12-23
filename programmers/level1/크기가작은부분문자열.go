bc9df38f161b19e3b5de7e4fd10a2427a7ac8240
thisisnew <thisisnew@naver.com>
2022-12-23 16:07:19 +0900

    크기가 작은 부분 문자열

---
+++ programmers/level1/크기가작은부분문자열.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(크기가작은부분문자열("3141592", "271"))
+}
+
+func 크기가작은부분문자열(t string, p string) int {
+
+	var result int
+	var ln = len([]rune(t))
+	for i := 0; i < ln; i++ {
+
+		if i > ln-3 {
+			break
+		}
+
+		s := t[i : i+3]
+
+		if s < p {
+			result++
+		}
+	}
+
+	return result
+}
