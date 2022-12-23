08e3f632b554cd7b4a2887eff365fddbace232b0
thisisnew <thisisnew@naver.com>
2022-12-24 00:15:40 +0900

    배달

--- programmers/level2/배달.go
+++ programmers/level2/배달.go
@@ -6,11 +6,28 @@ func main() {
 	fmt.Println(배달(5, [][]int{{1, 2, 1}, {2, 3, 3}, {5, 2, 2}, {1, 4, 2}, {5, 3, 1}, {5, 4, 2}}, 3))
 }
 
+var (
+	isVisitedDelivery []bool
+)
+
 func 배달(N int, road [][]int, k int) int {
 	answer := 0
 
 	// [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
 	fmt.Printf("Hello Go")
 
+	isVisitedDelivery = make([]bool, N)
+
 	return answer
+}
+
+func dfsDelivery(v int, road [][]int) {
+	isVisitedDelivery[v] = true
+
+	for i := 0; i < len(road[v]); i++ {
+		if road[v][i] == 1 && !isVisitedDelivery[i] {
+			dfsDelivery(i, road)
+		}
+	}
+
 }
