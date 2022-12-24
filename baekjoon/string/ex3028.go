e156e76c07124bdc74e23ff0ade3aa5268113cf6
thisisnew <thisisnew@naver.com>
2022-12-24 12:32:08 +0900

    창영마을

---
+++ baekjoon/string/ex3028.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	var order string
+	var read = bufio.NewReader(os.Stdin)
+	fmt.Fscanln(read, &order)
+
+	var cups = []bool{true, false, false}
+
+	for _, o := range order {
+		switch o {
+		case 'A':
+			cups[0], cups[1] = cups[1], cups[0]
+		case 'B':
+			cups[1], cups[2] = cups[2], cups[1]
+		case 'C':
+			cups[0], cups[2] = cups[2], cups[0]
+		}
+	}
+
+}
