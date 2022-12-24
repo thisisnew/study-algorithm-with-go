499c12b7d0ed0913877b716598b47e33a9cec47a
thisisnew <thisisnew@naver.com>
2022-12-24 13:22:30 +0900

    성택이의 은밀한 비밀번호

--- baekjoon/string/ex25372.go
+++ baekjoon/string/ex25372.go
@@ -12,10 +12,16 @@ func main() {
 	var read = bufio.NewReader(os.Stdin)
 	fmt.Fscanln(read, &n)
 
+	var pws = make([]string, n)
+
 	for i := 0; i < n; i++ {
 		var pw string
 		fmt.Fscanln(read, &pw)
 
+		pws[i] = pw
+	}
+
+	for _, pw := range pws {
 		fmt.Println(isValidPassword(pw))
 	}
 
