135d614a5b99653f6298ccfffecd18f026b5c2ee
thisisnew <thisisnew@naver.com>
2022-12-28 00:20:06 +0900

    수 이어 쓰기 3

---
+++ baekjoon/string/ex2154.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	var n int
+	var read = bufio.NewReader(os.Stdin)
+	fmt.Fscanln(read, &n)
+
+	for i := 1; i <= 100000; i++ {
+
+	}
+
+}
