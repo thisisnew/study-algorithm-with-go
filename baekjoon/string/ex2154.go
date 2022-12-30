b4217beb27cceb7c5551fd765bd3a7bac19e8732
thisisnew <thisisnew@naver.com>
2022-12-30 16:51:39 +0900

    수 이어 쓰기 3

--- baekjoon/string/ex2154.go
+++ baekjoon/string/ex2154.go
@@ -14,33 +14,46 @@ func main() {
 	var read = bufio.NewReader(os.Stdin)
 	fmt.Fscanln(read, &n)
 
+	total, totalLen := getTotalNumberString()
+	nLen := len([]rune(n))
 	var sb strings.Builder
-	var temp strings.Builder
 
-	for i := 1; i <= 10000-len([]rune(n)); i++ {
+	for i := 1; i <= totalLen-nLen; i++ {
 
-		d := strconv.Itoa(i)
-		temp.WriteString(d)
-
-		if d[0:1] != n[0:1] {
+		if total[i:i+1] != n[0:1] {
 			continue
 		}
 
-		sb.WriteString(d)
+		sb.WriteString(total[i : i+1])
 
-		for j := i + 1; j <= 10000; j++ {
-			if sb.Len() >= len([]rune(n)) {
-				if sb.String() == n {
-					fmt.Println(temp.Len() - (len([]rune(n)) - 1))
-					return
-				}
+		for j := i + 1; j <= totalLen; j++ {
 
+			if sb.Len() < nLen {
+				sb.WriteString(total[j : j+1])
+				continue
+			}
+
+			if sb.String() != n {
 				sb.Reset()
 				break
 			}
 
-			sb.WriteString(strconv.Itoa(j))
+			fmt.Println(i)
+			return
 		}
 	}
 
+}
+
+func getTotalNumberString() (string, int) {
+
+	var result strings.Builder
+
+	for i := 1; i <= 10000; i++ {
+
+		d := strconv.Itoa(i)
+		result.WriteString(d)
+	}
+
+	return result.String(), result.Len()
 }
