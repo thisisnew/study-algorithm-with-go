7c681c744a47f26cfa8f3d94b31953e4ddb181dd
thisisnew <thisisnew@naver.com>
2022-12-31 00:03:58 +0900

    마법의 엘리베이터

---
+++ programmers/level2/마법의엘리베이터.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(마법의엘리베이터(16))
+}
+
+func 마법의엘리베이터(storey int) int {
+
+	var div = 100000000
+	var result int
+
+	for {
+
+		if storey == 0 {
+			return result
+		}
+
+		if storey/div == 0 {
+			div = div / 10
+			continue
+		}
+
+		result += storey / div
+		storey = storey % div
+
+	}
+
+}
