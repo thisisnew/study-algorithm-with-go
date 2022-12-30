f0dc9e58a21d76fe4c86579f3fdbe609fb89ad0a
thisisnew <thisisnew@naver.com>
2022-12-30 17:00:02 +0900

    수 이어 쓰기 3

--- baekjoon/string/ex2154.go
+++ baekjoon/string/ex2154.go
@@ -10,50 +10,21 @@ import (
 
 func main() {
 
-	var n string
+	var n int
 	var read = bufio.NewReader(os.Stdin)
 	fmt.Fscanln(read, &n)
 
-	total, totalLen := getTotalNumberString()
-	nLen := len([]rune(n))
-	var sb strings.Builder
-
-	for i := 0; i < totalLen-nLen; i++ {
-
-		if total[i:i+1] != n[0:1] {
-			continue
-		}
-
-		sb.WriteString(total[i : i+1])
-
-		for j := i + 1; j <= totalLen; j++ {
-
-			if sb.Len() < nLen {
-				sb.WriteString(total[j : j+1])
-				continue
-			}
-
-			if sb.String() != n {
-				sb.Reset()
-				break
-			}
-
-			fmt.Println(i + 1)
-			return
-		}
-	}
-
+	fmt.Println(strings.Index(getTotalNumberString(n), strconv.Itoa(n)) + 1)
 }
 
-func getTotalNumberString() (string, int) {
+func getTotalNumberString(n int) string {
 
 	var result strings.Builder
 
-	for i := 1; i <= 10000; i++ {
-
+	for i := 1; i <= n; i++ {
 		d := strconv.Itoa(i)
 		result.WriteString(d)
 	}
 
-	return result.String(), result.Len()
+	return result.String()
 }
