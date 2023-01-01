8670204e2602d5567cf15c626a088d35ace44a49
thisisnew <thisisnew@naver.com>
2023-01-02 00:01:09 +0900

    문자열

--- baekjoon/string/ex1120.go
+++ baekjoon/string/ex1120.go
@@ -26,3 +26,20 @@ func main() {
 	}
 
 }
+
+func getDifBetweenTwoWords(a, b string) int {
+
+	if len([]rune(a)) > len([]rune(b)) {
+		a, b = b, a
+	}
+
+	var result int
+
+	for i := 0; i < len([]rune(a)); i++ {
+		if a[i:i+1] != b[i:i+1] {
+			result++
+		}
+	}
+
+	return result
+}
