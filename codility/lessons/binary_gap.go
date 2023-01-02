64ee239f2149afe2741095ad00ea50cfa4a187bb
thisisnew <thisisnew@naver.com>
2023-01-02 12:28:06 +0900

    binary gap

--- codility/lessons/binary_gap.go
+++ codility/lessons/binary_gap.go
@@ -12,15 +12,10 @@ func main() {
 
 func binaryGap(N int) int {
 
-	var result = 0
 	var binNum = strconv.FormatInt(int64(N), 2)
-
-	if strings.Count(binNum, "1") < 2 {
-		return result
-	}
-
 	var idx = strings.Index(binNum, "1")
 	var zeros = 0
+	var result = 0
 
 	for {
 		idx++
