e242448fc1880adde9ebec2a31f92dea82c31bb6
thisisnew <thisisnew@naver.com>
2023-01-03 00:48:27 +0900

    ParkingBill

--- codility/Exercises/parking_bill.go
+++ codility/Exercises/parking_bill.go
@@ -1,13 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+const parkingTimeLayout = "15:04:05"
 
 func main() {
 	fmt.Println(ParkingBill("10:00", "13:21"))
 }
 
 func ParkingBill(E string, L string) int {
-	var result int
+	var result = 2
+
+	eTime, _ := time.Parse(parkingTimeLayout, E)
+	lTime, _ := time.Parse(parkingTimeLayout, L)
+
+	diff := lTime.Sub(eTime)
+
+	difMin := diff.Minutes()
+
+	fmt.Println(difMin)
 
 	return result
 }
