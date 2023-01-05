ba7e3865645d3acdc24ee9bbcba69597099116c5
thisisnew <thisisnew@naver.com>
2023-01-05 16:21:13 +0900

    StrSymmetryPoint

--- codility/Exercises/str_symmetry_point.go
+++ codility/Exercises/str_symmetry_point.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	fmt.Println(StrSymmetryPoint("racecar"))
@@ -8,9 +11,29 @@ func main() {
 
 func StrSymmetryPoint(S string) int {
 
-	if len([]rune(S)) <= 1 {
-		return 0
+	var result = 0
+	var ln = len([]rune(S))
+
+	if ln <= 1 {
+		return result
 	}
 
-	return len([]rune(S)) / 2
+	mid := len([]rune(S)) / 2
+
+	if S[0:mid+1] != getReverserStr(mid, S) {
+		return -1
+	}
+
+	return mid
+}
+
+func getReverserStr(idx int, S string) string {
+
+	var result strings.Builder
+
+	for i := len([]rune(S)) - 1; i >= idx; i-- {
+		result.WriteString(S[i : i+1])
+	}
+
+	return result.String()
 }
