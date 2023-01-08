037dfdc0d95f0744a4efc170e34eafa35e485e7a
thisisnew <thisisnew@naver.com>
2023-01-09 05:46:30 +0900

    Pen Pineapple Apple Pen

--- baekjoon/string/ex15881.go
+++ baekjoon/string/ex15881.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -13,5 +14,20 @@ func main() {
 
 	fmt.Fscanln(read, &n)
 	fmt.Fscanln(read, &input)
+
+	var idx = -1
+	var result = 0
+
+	for {
+		idx = strings.Index(input, "pPAp")
+
+		if idx < 0 || len([]rune(input)) < 4 {
+			fmt.Println(result)
+			return
+		}
+
+		result++
+		input = input[0:idx] + input[idx+4:]
+	}
 
 }
